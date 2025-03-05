5096244f0f2cfa974122b25fcb39673f7bb80584
명훈 <s23065@gsm.hs.kr>
2025-03-05 20:18:32 +0900

    :lipstick: :: Add Splash Logo Component

---
+++ lib/presentation/splash/widgets/splash_logo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:gogo_app/core/design_system/theme/icon.dart';
+
+class Logo extends StatelessWidget {
+  const Logo();
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+        tag: 'gogo_splash_icon',
+        child: GogoIcons.logo(
+          width: 210,
+          height: 72
+        )
+    );
+  }
+}
