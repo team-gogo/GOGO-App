c1506ba5ffefb564ff83eb4dec8e8bcbff8556d4
youloveli <mymhj122@gmail.com>
2025-03-06 00:46:55 +0900

    :sparkles: :: login_state.dart 추가

---
+++ lib/presentation/logIn/bloc/login_state.dart
@@ -0,0 +1,11 @@
+abstract class LoginState {}
+
+class Init extends LoginState {}
+
+class GoogleLoginFail extends LoginState {}
+
+class GogoLoginFail extends LoginState {}
+
+class GoogleLoginSuccess extends LoginState {}
+
+class GogoLoginSuccess extends LoginState {}
