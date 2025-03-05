3f25fdeea4f2e0bd392f45e7a69a05984e94c61c
parkyuhyeon <pyh1111802@naver.com>
2025-03-05 13:34:22 +0900

    :lipstick: :: Unfocus 적용 및 불필요한 코드 삭제

--- lib/presentation/sign_up/widgets/page/name_page.dart
+++ lib/presentation/sign_up/widgets/page/name_page.dart
@@ -11,40 +11,38 @@ class NamePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: GogoColors.black,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(16, 50, 16, 95),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              GogoIcons.chevronLeft(
-                color: GogoColors.white,
-                width: 40,
-                height: 40,
-                onTap: () {},
-              ),
-              SizedBox(height: 40),
-              Text(
-                "이름을 알려주세요.",
-                style: GogoTypography.title3Extrabold.copyWith(color: GogoColors.white),
-              ),
-              SizedBox(height: 36,),
-              GogoTextField(
-                textFieldState: GogoTextFieldState.basic,
-                controller: TextEditingController(),
-                hintText: "이름를 입력해주세요.",
-                contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                borderRadius: BorderRadius.all(Radius.circular(12)),
-                backgroundColor: GogoColors.gray700,
-                textStyle: GogoTypography.body3Semibold,
-                hintStyle: GogoTypography.body3Semibold,
-              ),
-              Spacer(),
-              GogoDefaultButton(
-                  onTap: () => FocusScope.of(context).unfocus(), text: "다음")
-            ],
+    return GestureDetector(
+      onTap: ()=>FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: GogoColors.black,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16, 50, 16, 95),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GogoIcons.chevronLeft(
+                  color: GogoColors.white,
+                  width: 40,
+                  height: 40,
+                  onTap: () {},
+                ),
+                SizedBox(height: 40),
+                Text(
+                  "이름을 알려주세요.",
+                  style: GogoTypography.title3Extrabold.copyWith(color: GogoColors.white),
+                ),
+                SizedBox(height: 36,),
+                GogoTextField(
+                  textFieldState: GogoTextFieldState.basic,
+                  controller: TextEditingController(),
+                  hintText: "이름를 입력해주세요.",
+                ),
+                Spacer(),
+                GogoDefaultButton(
+                    onTap: () {}, text: "다음")
+              ],
+            ),
           ),
         ),
       ),
