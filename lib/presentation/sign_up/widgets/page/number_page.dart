3c5da116851e94c2904052bd9b1999107a524e8d
parkyuhyeon <pyh1111802@naver.com>
2025-03-05 10:52:53 +0900

    :lipstick: :: Publishing Number Page

---
+++ lib/presentation/sign_up/widgets/page/number_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:gogo_app/core/design_system/component/textField/gogo_text_field.dart';
+import 'package:gogo_app/core/design_system/theme/color.dart';
+import 'package:gogo_app/core/design_system/theme/typography.dart';
+import '../../../../core/design_system/component/button/gogo_default_button.dart';
+import '../../../../core/design_system/theme/icon.dart';
+
+class NumberPage extends StatelessWidget {
+  const NumberPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(16, 50, 16, 93),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GogoIcons.chevronLeft(
+                  color: GogoColors.white,
+                  width: 40,
+                  height: 40,
+                  onTap: () {},
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  '반과 번호를 알려주세요',
+                  style: GogoTypography.title4Extrabold
+                      .copyWith(color: GogoColors.white),
+                ),
+                SizedBox(
+                  height: 36,
+                ),
+                GogoTextField(
+                  textFieldState: GogoTextFieldState.basic,
+                  controller: TextEditingController(),
+                  hintText: "학년",
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  backgroundColor: GogoColors.gray700,
+                  textStyle: GogoTypography.body3Semibold,
+                  hintStyle: GogoTypography.body3Semibold,
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                GogoTextField(
+                  textFieldState: GogoTextFieldState.basic,
+                  controller: TextEditingController(),
+                  hintText: "반",
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  backgroundColor: GogoColors.gray700,
+                  textStyle: GogoTypography.body3Semibold,
+                  hintStyle: GogoTypography.body3Semibold,
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                GogoTextField(
+                  textFieldState: GogoTextFieldState.basic,
+                  controller: TextEditingController(),
+                  hintText: "번호",
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  backgroundColor: GogoColors.gray700,
+                  textStyle: GogoTypography.body3Semibold,
+                  hintStyle: GogoTypography.body3Semibold,
+                ),
+                Spacer(),
+                GogoDefaultButton(onTap: () {}, text: "다음")
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
