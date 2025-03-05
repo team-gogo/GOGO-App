d33240f697a914698726a04ab85fb0d7b83744f3
youloveli <mymhj122@gmail.com>
2025-03-06 01:08:19 +0900

    :recycle: :: GoogleLoginFail과 GoogleLoginSuccess에 각각 message, user 필드 추가

--- lib/presentation/logIn/bloc/login_state.dart
+++ lib/presentation/logIn/bloc/login_state.dart
@@ -1,11 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 abstract class LoginState {}
 
 class Init extends LoginState {}
 
-class GoogleLoginFail extends LoginState {}
+class GoogleLoginFail extends LoginState {
+  final String message;
+
+  GoogleLoginFail({required this.message});
+}
 
 class GogoLoginFail extends LoginState {}
 
-class GoogleLoginSuccess extends LoginState {}
+class GoogleLoginSuccess extends LoginState {
+  final User user;
+
+  GoogleLoginSuccess({required this.user});
+}
 
 class GogoLoginSuccess extends LoginState {}
