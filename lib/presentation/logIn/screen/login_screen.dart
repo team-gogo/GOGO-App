55f1c6892f1c8292ab3affdcd5e120471ee1ab92
youloveli <mymhj122@gmail.com>
2025-03-06 01:09:42 +0900

    :sparkles: :: 블록 적용, 에러 핸들링 추가

--- lib/presentation/logIn/screen/login_screen.dart
+++ lib/presentation/logIn/screen/login_screen.dart
@@ -1,77 +1,60 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gogo_app/presentation/logIn/bloc/login_bloc.dart';
+import 'package:gogo_app/presentation/logIn/bloc/login_event.dart';
+import 'package:gogo_app/presentation/logIn/bloc/login_state.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:gogo_app/presentation/sign_up/screen/sign_up_screen.dart';
 import '../../../core/design_system/theme/color.dart';
 import '../../../core/design_system/theme/icon.dart';
 import '../../logIn/widgets/google_login_button.dart';
 
 class LogInScreen extends StatelessWidget {
-  LogInScreen({super.key});
-
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  Future<User?> signInWithGoogle() async {
-    try {
-      // Google 로그인
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      if (googleUser == null) {
-        return null; // 로그인 취소
-      }
-
-      // Google 인증을 통해 ID 토큰과 인증 정보를 받음
-      final GoogleSignInAuthentication googleAuth =
-          await googleUser.authentication;
-
-      // Firebase에 Google 인증 정보를 전달하여 로그인
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-
-      // Firebase에서 로그인
-      final UserCredential userCredential =
-          await _auth.signInWithCredential(credential);
-      return userCredential.user;
-    } catch (e) {
-      print("Error during Google sign-in: $e");
-      return null;
-    }
-  }
+  const LogInScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: GogoColors.black),
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(16, 0, 16, 111),
-            child: SizedBox(
-              width: double.infinity,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: GoogleLoginButton(onPressed: () async {
-                  User? user = await signInWithGoogle();
-                  if (user != null) {
-                    // 로그인 성공
-                    print('Signed in as ${user.displayName}');
-                  } else {
-                    // 로그인 실패
-                    print('Sign-in failed');
-                  }
-                }),
+    return BlocConsumer<LoginBloc, LoginState>(
+      listener: (context, state) {
+        if (state is GoogleLoginFail) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("구글 로그인에 실패 했습니다")),
+          );
+        }
+        if (state is GoogleLoginSuccess) {
+          SnackBar(
+            content: Text("${state.user.displayName}으로 구글 로그인에 성공0 했습니다"),
+          );
+        }
+      },
+      builder: (context, state) {
+        return Container(
+          decoration: BoxDecoration(color: GogoColors.black),
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(16, 0, 16, 111),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: GoogleLoginButton(
+                      onPressed: () async {
+                        context.read<LoginBloc>().add(GoogleLogInEvent());
+                      },
+                    ),
+                  ),
+                ),
               ),
-            ),
+              Center(
+                child: GogoIcons.logo(width: double.infinity, height: 80),
+              ),
+            ],
           ),
-          Center(
-            child: GogoIcons.logo(width: double.infinity, height: 80),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
