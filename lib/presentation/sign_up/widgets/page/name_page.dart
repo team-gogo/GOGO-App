fa4531b194746c574631ab92c7b4b79aa4e8ab6e
parkyuhyeon <pyh1111802@naver.com>
2025-03-05 19:56:07 +0900

    :sparkles: :: Name페이지 기능 추가

--- lib/presentation/sign_up/widgets/page/name_page.dart
+++ lib/presentation/sign_up/widgets/page/name_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gogo_app/presentation/sign_up/bloc/name/name_bloc.dart';
+import 'package:gogo_app/presentation/sign_up/bloc/name/name_state.dart';
 import '../../../../core/design_system/component/button/gogo_default_button.dart';
 import '../../../../core/design_system/component/text_field/gogo_text_field.dart';
 import '../../../../core/design_system/theme/color.dart';
@@ -6,45 +9,54 @@ import '../../../../core/design_system/theme/icon.dart';
 import '../../../../core/design_system/theme/typography.dart';
 
 class NamePage extends StatelessWidget {
-  const NamePage({super.key});
+  final VoidCallback onBackClick;
+  final VoidCallback onNextClick;
+
+  const NamePage({
+    super.key,
+    required this.onBackClick,
+    required this.onNextClick,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: ()=>FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        backgroundColor: GogoColors.black,
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(16, 50, 16, 95),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                GogoIcons.chevronLeft(
-                  color: GogoColors.white,
-                  width: 40,
-                  height: 40,
-                  onTap: () {},
-                ),
-                SizedBox(height: 40),
-                Text(
-                  "이름을 알려주세요.",
-                  style: GogoTypography.title3Extrabold.copyWith(color: GogoColors.white),
-                ),
-                SizedBox(height: 36,),
-                GogoTextField(
-                  textFieldState: GogoTextFieldState.basic,
-                  controller: TextEditingController(),
-                  hintText: "이름를 입력해주세요.",
-                ),
-                Spacer(),
-                GogoDefaultButton(
-                    onTap: () {}, text: "다음")
-              ],
+    return BlocBuilder<NameBloc, NameState>(
+      builder: (context, state) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GogoIcons.chevronLeft(
+              color: GogoColors.white,
+              width: 40,
+              height: 40,
+              onTap: onBackClick,
             ),
-          ),
-        ),
-      ),
+            SizedBox(height: 40),
+            Text(
+              "이름을 알려주세요.",
+              style:
+                  GogoTypography.title3Extrabold.copyWith(color: GogoColors.white),
+            ),
+            SizedBox(
+              height: 36,
+            ),
+            GogoTextField(
+              textFieldState: GogoTextFieldState.basic,
+              controller: context.read<NameBloc>().nameController,
+              hintText: "이름를 입력해주세요.",
+              validator: context.read<NameBloc>().validateName,
+            ),
+            Spacer(),
+            GogoDefaultButton(
+              onTap: state is EnableNameState ? onNextClick : () {},
+              text: "다음",
+              color: state is EnableNameState
+                  ? GogoColors.main600
+                  : GogoColors.gray400,
+            ),
+          ],
+        );
+      }
     );
   }
 }
