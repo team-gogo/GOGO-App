72be0c5d407bfd86c735f9f5f6d17c5c06c0ba36
parkyuhyeon <pyh1111802@naver.com>
2025-03-05 19:59:52 +0900

    :sparkles: :: Sex페이지 기능 추가

--- lib/presentation/sign_up/widgets/page/sex_page.dart
+++ lib/presentation/sign_up/widgets/page/sex_page.dart
@@ -1,47 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:gogo_app/presentation/sign_up/bloc/sex/sex_bloc.dart';
 import '../../../../core/design_system/component/button/gogo_default_button.dart';
 import '../../../../core/design_system/theme/color.dart';
 import '../../../../core/design_system/theme/icon.dart';
 import '../../../../core/design_system/theme/typography.dart';
+import '../../bloc/sex/sex_state.dart';
 
 class SexPage extends StatelessWidget {
-  const SexPage({super.key});
+  const SexPage({
+    super.key,
+    required this.onBackClick,
+    required this.onNextClick,
+  });
+
+  final VoidCallback onBackClick;
+  final VoidCallback onNextClick;
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(16, 50, 16, 93),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              GogoIcons.chevronLeft(
-                color: GogoColors.white,
-                width: 40,
-                height: 40,
-                onTap: () {},
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Text(
-                '반과 번호를 알려주세요',
-                style: GogoTypography.title4Extrabold
-                    .copyWith(color: GogoColors.white),
-              ),
-              SizedBox(
-                height: 36,
-              ),
-              GogoDefaultButton(onTap: (){}, text: "남성"),
-              SizedBox(height: 12,),
-              GogoDefaultButton(onTap: (){}, text: "여성"),
-              Spacer(),
-              GogoDefaultButton(onTap: () {}, text: "다음")
-            ],
-          ),
-        ),
-      ),
+    return BlocBuilder<SexBloc, SexState>(
+      builder: (context, state) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GogoIcons.chevronLeft(
+              color: GogoColors.white,
+              width: 40,
+              height: 40,
+              onTap: onBackClick,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              '반과 번호를 알려주세요',
+              style:
+                  GogoTypography.title4Extrabold.copyWith(color: GogoColors.white),
+            ),
+            SizedBox(
+              height: 36,
+            ),
+            GogoDefaultButton(
+              onTap: ()=> context.read<SexBloc>().sexController = Sex.male,
+              text: "남성",
+              color: state is EnableMaleSexState
+                  ? GogoColors.main600
+                  : GogoColors.gray400,
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            GogoDefaultButton(
+              onTap: ()=>context.read<SexBloc>().sexController = Sex.female,
+              text: "여성",
+              color: state is EnableFemaleSexState
+                  ? GogoColors.main600
+                  : GogoColors.gray400,
+            ),
+            Spacer(),
+            GogoDefaultButton(
+              onTap: state is EnableSexState ? onNextClick : () {},
+              text: "다음",
+              color: state is EnableSexState
+                  ? GogoColors.main600
+                  : GogoColors.gray400,
+            ),
+          ],
+        );
+      }
     );
   }
 }
