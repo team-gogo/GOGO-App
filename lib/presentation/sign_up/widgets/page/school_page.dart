640fdb2579b3218b919a6a1d872d33a59e6d02e0
parkyuhyeon <pyh1111802@naver.com>
2025-03-05 19:59:19 +0900

    :sparkles: :: School페이지 기능 추가

--- lib/presentation/sign_up/widgets/page/school_page.dart
+++ lib/presentation/sign_up/widgets/page/school_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gogo_app/presentation/sign_up/bloc/school/school_bloc.dart';
+import 'package:gogo_app/presentation/sign_up/bloc/school/school_state.dart';
 import '../../../../core/design_system/component/button/gogo_default_button.dart';
 import '../../../../core/design_system/component/text_field/gogo_text_field.dart';
 import '../../../../core/design_system/theme/color.dart';
@@ -6,47 +9,50 @@ import '../../../../core/design_system/theme/icon.dart';
 import '../../../../core/design_system/theme/typography.dart';
 
 class SchoolPage extends StatelessWidget {
-  const SchoolPage({super.key});
+  const SchoolPage({
+    super.key,
+    required this.onBackClick,
+    required this.onNextClick,
+  });
+
+  final VoidCallback onBackClick;
+  final VoidCallback onNextClick;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: ()=> FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(16, 50, 16, 95),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                GogoIcons.chevronLeft(
-                  color: GogoColors.white,
-                  width: 40,
-                  height: 40,
-                  onTap: () {},
-                ),
-                SizedBox(height: 40),
-                Text(
-                  "학교를 알려주세요.",
-                  style: GogoTypography.title3Extrabold
-                      .copyWith(color: GogoColors.white),
-                ),
-                SizedBox(
-                  height: 36,
-                ),
-                GogoTextField(
-                  textFieldState: GogoTextFieldState.search,
-                  controller: TextEditingController(),
-                  hintText: "학교를 입력해주세요.",
-                ),
-                Spacer(),
-                GogoDefaultButton(
-                    onTap: () {}, text: "다음")
-              ],
-            ),
+    return BlocBuilder<SchoolBloc, SchoolState>(builder: (context, state) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GogoIcons.chevronLeft(
+              color: GogoColors.white,
+              width: 40,
+              height: 40,
+              onTap: onBackClick),
+          SizedBox(height: 40),
+          Text(
+            "학교를 알려주세요.",
+            style: GogoTypography.title3Extrabold
+                .copyWith(color: GogoColors.white),
           ),
-        ),
-      ),
-    );
+          SizedBox(
+            height: 36,
+          ),
+          GogoTextField(
+            textFieldState: GogoTextFieldState.search,
+            controller: context.read<SchoolBloc>().schoolController,
+            hintText: "학교를 입력해주세요.",
+          ),
+          Spacer(),
+          GogoDefaultButton(
+            onTap: state is EnableSchoolState ? onNextClick : () {},
+            text: "다음",
+            color: state is EnableSchoolState
+                ? GogoColors.main600
+                : GogoColors.gray400,
+          ),
+        ],
+      );
+    });
   }
 }
