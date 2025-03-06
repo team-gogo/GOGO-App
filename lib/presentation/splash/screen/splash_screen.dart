dd155030be02e75a0a744dde2656e90990422d8f
명훈 <s23065@gsm.hs.kr>
2025-03-06 09:02:38 +0900

    :recycle: :: Apply Bloc -> SplashScreen

--- lib/presentation/splash/screen/splash_screen.dart
+++ lib/presentation/splash/screen/splash_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gogo_app/core/design_system/theme/color.dart';
+import 'package:gogo_app/presentation/splash/bloc/splash_bloc.dart';
+import 'package:gogo_app/presentation/splash/bloc/splash_event.dart';
+import 'package:gogo_app/presentation/splash/bloc/splash_state.dart';
 import 'package:gogo_app/presentation/splash/widgets/splash_logo.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -7,17 +11,24 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: GogoColors.black,
-      body: Center(
-        child: TweenAnimationBuilder<double>(
-          tween: Tween(begin: 0.86, end: 1.0),
-          duration: const Duration(seconds: 2),
-          curve: Curves.easeInOut,
-          builder: (context, value, child) {
-            return Transform.scale(scale: value, child: child);
-          },
-          child: Logo()
+    return BlocProvider(
+      create: (BuildContext context) =>
+          SplashBloc()..add(LaunchSplashEvent()),
+      child: BlocListener<SplashBloc, SplashState>(
+        listener: (context, state) {},
+        child: Scaffold(
+          backgroundColor: GogoColors.black,
+          body: Center(
+            child: TweenAnimationBuilder<double>(
+                tween: Tween(begin: 0.86, end: 1.0),
+                duration: const Duration(seconds: 2),
+                curve: Curves.easeInOut,
+                builder: (context, value, child) {
+                  return Transform.scale(scale: value, child: child);
+                },
+                child: Logo()
+            ),
+          ),
         ),
       ),
     );
