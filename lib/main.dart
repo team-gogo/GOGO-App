966412a1a7a95297eee47e83d34b1fb001c76f82
youloveli <mymhj122@gmail.com>
2025-03-06 17:25:27 +0900

    :sparkles: :: 의존성 주입 모듈 초기화를 위해 setupLocator를 main 함수에서 사용

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gogo_app/core/design_system/theme/color.dart';
 import 'package:gogo_app/router.dart';
+import 'data/getItModule/get_it_module.dart';
 import 'firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 파이어베이스 초기화
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // get it 초기화(의존성 주입)
+  setupLocator();
   runApp(const MyApp());
 }
 
