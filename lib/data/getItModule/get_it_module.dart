f1844624c62eb40a9e1530020294196d18f65fba
youloveli <mymhj122@gmail.com>
2025-03-06 17:18:46 +0900

    :sparkles: :: setupLocator 에 Dio 객체와 AuthApi 객체 추가

--- lib/data/getItModule/get_it_module.dart
+++ lib/data/getItModule/get_it_module.dart
@@ -1,5 +1,19 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:gogo_app/data/service/auth_api.dart';
 
-final getIt = GetIt.instance;
+final locator = GetIt.instance;
 
-void setupLocator() {}
+void setupLocator() {
+  locator.registerLazySingleton<Dio>(
+    () => Dio(
+      BaseOptions(
+        baseUrl: 'https://api.baseURL.com',
+        connectTimeout: Duration(seconds: 30),
+        receiveTimeout: Duration(seconds: 30),
+      ),
+    ),
+  );
+
+  locator.registerLazySingleton<AuthApi>(() => AuthApi(locator<Dio>()));
+}
