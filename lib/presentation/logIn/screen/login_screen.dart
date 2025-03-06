c368b8d2e0d7e5cd84c5845b4314c01a7d8ebe85
youloveli <mymhj122@gmail.com>
2025-03-06 14:37:54 +0900

    :recycle: :: Bloc연결

--- lib/presentation/logIn/screen/login_screen.dart
+++ lib/presentation/logIn/screen/login_screen.dart
@@ -1,12 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gogo_app/presentation/logIn/bloc/login_bloc.dart';
 import 'package:gogo_app/presentation/logIn/bloc/login_event.dart';
 import 'package:gogo_app/presentation/logIn/bloc/login_state.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:flutter/cupertino.dart';
-import '../../../core/design_system/theme/color.dart';
+
 import '../../../core/design_system/theme/icon.dart';
 import '../../logIn/widgets/google_login_button.dart';
 
@@ -15,46 +12,34 @@ class LogInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<LoginBloc, LoginState>(
-      listener: (context, state) {
-        if (state is GoogleLoginFail) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text("구글 로그인에 실패 했습니다")),
-          );
-        }
-        if (state is GoogleLoginSuccess) {
-          SnackBar(
-            content: Text("${state.user.displayName}으로 구글 로그인에 성공0 했습니다"),
-          );
-        }
-      },
-      builder: (context, state) {
-        return Container(
-          decoration: BoxDecoration(color: GogoColors.black),
+    return BlocProvider(
+      create: (context) => LoginBloc(),
+      child: BlocListener<LoginBloc, LoginState>(
+          listener: (BuildContext context, LoginState state) {
+            // 네비게이션 세팅
+          },
           child: Stack(
-            alignment: Alignment.bottomCenter,
             children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(16, 0, 16, 111),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: GoogleLoginButton(
-                      onPressed: () async {
-                        context.read<LoginBloc>().add(GoogleLogInEvent());
-                      },
-                    ),
-                  ),
-                ),
-              ),
               Center(
                 child: GogoIcons.logo(width: double.infinity, height: 80),
               ),
+              Positioned(
+                bottom: 111,
+                left: 16,
+                right: 16,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: GoogleLoginButton(
+                    onPressed: () {
+                      BlocProvider.of<LoginBloc>(context)
+                          .add(GoogleLogInEvent());
+                    },
+                  ),
+                ),
+              ),
             ],
-          ),
-        );
-      },
+          );
+      ),
     );
   }
 }
