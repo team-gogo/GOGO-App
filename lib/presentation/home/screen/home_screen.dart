3e8d59b265fc4bfe299e6cd4e9ff965ad9781bf9
명훈 <s23065@gsm.hs.kr>
2025-03-06 19:50:49 +0900

    :recycle: :: Refactor Navigation Views(Home, Notification, Profile, Stage)

--- lib/presentation/home/screen/home_screen.dart
+++ lib/presentation/home/screen/home_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../../../design_system/theme/color.dart';
+import '../../../design_system/theme/typography.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text("Home Screen"),);
-  }
+    return Scaffold(
+        body: Center(
+          child: Text(
+              style: GogoTypography.title3Extrabold.copyWith(color: GogoColors.white),
+              "Home"
+          ),
+        )
+    );  }
 }
--- lib/presentation/notification/screen/notification_screen.dart
+++ lib/presentation/notification/screen/notification_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../../../design_system/theme/color.dart';
+import '../../../design_system/theme/typography.dart';
+
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text("Notification Screen"));
-  }
+    return Scaffold(
+        body: Center(
+          child: Text(
+              style: GogoTypography.title3Extrabold.copyWith(color: GogoColors.white),
+              "Notification"
+          ),
+        )
+    );  }
 }
