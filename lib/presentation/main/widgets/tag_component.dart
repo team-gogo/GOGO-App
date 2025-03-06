ebf53e279850845588e059e89e314738c01f906b
parkyuhyeon <pyh1111802@naver.com>
2025-03-06 20:15:17 +0900

    :lipstick: :: 태그 컴포넌트 퍼블리싱

--- lib/presentation/main/widgets/tag_component.dart
+++ lib/presentation/main/widgets/tag_component.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:gogo_app/core/design_system/theme/typography.dart';
+import '../../../design_system/theme/typography.dart';
 
-abstract class DefaultTagComponent extends StatelessWidget {
+enum TagState { basic, isSelected }
+
+class TagComponent extends StatelessWidget {
   final Color color;
   final double borderWidth;
   final BorderRadius borderRadius;
   final EdgeInsetsGeometry padding;
-  final Widget icon;
+  final Widget? icon;
   final String text;
   final TextStyle textStyle;
+  final TagState tagState;
 
-  const DefaultTagComponent({
+  const TagComponent({
     super.key,
     required this.color,
-    required this.icon,
+    this.icon,
     required this.text,
+    this.tagState = TagState.basic,
     this.borderWidth = 1,
     this.borderRadius = const BorderRadius.all(Radius.circular(12)),
     this.padding = const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
@@ -26,17 +30,24 @@ abstract class DefaultTagComponent extends StatelessWidget {
     return Container(
       padding: padding,
       decoration: BoxDecoration(
-          border: Border.all(
-            color: color,
-            width: borderWidth,
-          ),
+          border: tagState == TagState.isSelected
+              ? null
+              : Border.all(
+                  color: color,
+                  width: borderWidth,
+                ),
+          color: tagState == TagState.isSelected ? color : null,
           borderRadius: borderRadius),
       child: Row(
+        spacing: 8,
+        mainAxisSize: MainAxisSize.min,
         children: [
-          icon,
+          icon == null ? SizedBox() : icon!,
           Text(
             text,
-            style: textStyle.copyWith(color: color),
+            style: textStyle.copyWith(
+              color: tagState == TagState.isSelected ? null : color,
+            ),
           )
         ],
       ),
