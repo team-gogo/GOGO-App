e79097d3057889c8e15f353b739233665955439f
answad <mymhj122@gmail.com>
2025-03-06 18:27:32 +0900

    :twisted_rightwards_arrows: :: develop pull 컨플릭트 해결

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:gogo_app/core/design_system/theme/color.dart';
 import 'package:gogo_app/router.dart';
 import 'data/getItModule/get_it_module.dart';
+import 'design_system/theme/color.dart';
 import 'firebase_options.dart';
 
 void main() async {
