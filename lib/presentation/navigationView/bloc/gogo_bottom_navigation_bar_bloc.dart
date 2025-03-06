e22dd5ccc9f9d694da1fd0679d43bd51ac3e807e
명훈 <s23065@gsm.hs.kr>
2025-03-06 19:53:37 +0900

    :memo: :: Update

--- lib/presentation/navigationView/bloc/gogo_bottom_navigation_bar_bloc.dart
+++ lib/presentation/navigationView/bloc/gogo_bottom_navigation_bar_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
-import 'package:gogo_app/design_system/component/bottom_navigation_bar/bloc/gogo_bottom_navigation_bar_event.dart';
+
+import 'gogo_bottom_navigation_bar_event.dart';
 
 class GogoBottomNavigationBarBloc extends Bloc<GogoBottomNavigationBarEvent, int> {
   GogoBottomNavigationBarBloc() : super(0) {
