087bdd1f8f328cc1632bcc6d7f735b3a44a54376
parkyuhyeon <pyh1111802@naver.com>
2025-03-07 20:44:03 +0900

    :lipstick: :: HomeScreen에 EndedMatchCardComponent 추가

--- lib/presentation/home/screen/home_screen.dart
+++ lib/presentation/home/screen/home_screen.dart
@@ -19,33 +19,59 @@ class HomeScreen extends StatelessWidget {
                 color: GogoColors.main500,
                 icon: GogoIcons.volleyball(),
                 text: "배구"),
-            MatchCardComponent(
-              time: TagComponent.small(
-                color: GogoColors.success,
-                text: "12:00",
-                icon: GogoIcons.alarm(
-                  color: GogoColors.success,
-                  width: 12,
-                  height: 12,
-                ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  MatchCardComponent(
+                    time: TagComponent.small(
+                      color: GogoColors.success,
+                      text: "12:00",
+                      icon: GogoIcons.alarm(
+                        color: GogoColors.success,
+                        width: 12,
+                        height: 12,
+                      ),
+                    ),
+                    round: TagComponent.small(
+                      color: GogoColors.white,
+                      text: "12강",
+                      icon: GogoIcons.trophy(
+                          color: GogoColors.white, width: 12, height: 12),
+                    ),
+                    event: TagComponent.small(
+                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                      color: GogoColors.main500,
+                      text: "배드민턴",
+                      icon: GogoIcons.volleyball(
+                          color: GogoColors.main500, width: 12, height: 12),
+                    ),
+                    point: '10000P',
+                    teamA: "A",
+                    teamB: 'B',
+                  ),EndedMatchCardComponent(
+                    round: TagComponent.small(
+                      color: GogoColors.white,
+                      text: "12강",
+                      icon: GogoIcons.trophy(
+                          color: GogoColors.white, width: 12, height: 12),
+                    ),
+                    event: TagComponent.small(
+                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                      color: GogoColors.main500,
+                      text: "배드민턴",
+                      icon: GogoIcons.volleyball(
+                          color: GogoColors.main500, width: 12, height: 12),
+                    ),
+                    point: '10000P',
+                    winner: "A",
+                    earnedPoint: -1000,
+                    isPredictionSuccess: false,
+                  )
+                ],
               ),
-              round: TagComponent.small(
-                color: GogoColors.white,
-                text: "12강",
-                icon: GogoIcons.trophy(
-                    color: GogoColors.white, width: 12, height: 12),
-              ),
-              event: TagComponent.small(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                color: GogoColors.main500,
-                text: "배드민턴",
-                icon: GogoIcons.volleyball(
-                    color: GogoColors.main500, width: 12, height: 12),
-              ),
-              point: '10000P',
-              teamA: "A",
-              teamB: 'B',
-            )
+            ),
+
           ],
         ),
       ),
