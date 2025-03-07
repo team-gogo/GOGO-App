44126fd67b7df7bac84b98c519dbc10b27520230
answad <mymhj122@gmail.com>
2025-03-07 20:47:30 +0900

    :recycle: :: AuthDatasourceImpl이 AuthApi를 받도록 수정

--- lib/data/data_sources/login/auth_data_source_impl.dart
+++ lib/data/data_sources/login/auth_data_source_impl.dart
@@ -1,15 +1,13 @@
 import 'package:dio/dio.dart';
-
-import 'package:gogo_app/data/util/handle_dio_error.dart';
-
 import 'package:gogo_app/data/models/login/google_oauth/google_oauth_login_request.dart';
 import 'package:gogo_app/data/models/login/google_oauth/google_oauth_login_response.dart';
+import 'package:gogo_app/data/api/auth_api.dart';
 import 'auth_data_source.dart';
 
 class AuthDatasourceImpl implements AuthDatasource {
-  final Dio _dio;
+  final AuthApi _authApi;
 
-  AuthDatasourceImpl(this._dio);
+  AuthDatasourceImpl(Dio dio) : _authApi = AuthApi(dio);
 
   @override
   Future<GoogleOAuthLoginResponse> googleOAuthLogin(
