19d72e7affb9d4a212926cf9f390a1c54a65dfd1
parkyuhyeon <pyh1111802@naver.com>
2025-03-07 12:01:55 +0900

    :sparkles: :: Match_card bloc 기능구현

---
+++ lib/presentation/home/widgets/match_card/bloc/match_card_state.dart
@@ -0,0 +1,12 @@
+abstract class MatchCardAlertState {}
+
+class DisableMatchCardAlertState extends MatchCardAlertState {}
+
+class EnableMatchCardAlertState extends MatchCardAlertState {}
+
+
+abstract class MatchCardBettingState {}
+
+class DisableMatchCardBettingState extends MatchCardBettingState{}
+
+class EnableMatchCardBettingState extends MatchCardBettingState{}
