dd35c3647d965b541f47c7c63c8308cb6f819850
youloveli <mymhj122@gmail.com>
2025-03-08 22:26:40 +0900

    :sparkles: :: CommunitySearchRequestQueryString, sort_type.dart 추가

---
+++ lib/data/models/stage/community/community_search_request_query_string.dart
@@ -0,0 +1,27 @@
+import 'package:gogo_app/data/models/stage/community/sort_type.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import 'match_board_search_query_string.dart';
+
+part 'community_search_request_query_string.g.dart';
+
+@JsonSerializable()
+class CommunitySearchRequestQueryString {
+  final int page;
+  final int size;
+  final GameType type;
+  final SortType sort;
+
+  CommunitySearchRequestQueryString({
+    required this.page,
+    required this.size,
+    required this.type,
+    required this.sort,
+  });
+
+  factory CommunitySearchRequestQueryString.fromJson(
+          Map<String, dynamic> json) =>
+      _$CommunitySearchRequestQueryStringFromJson(json);
+  Map<String, dynamic> toJson() =>
+      _$CommunitySearchRequestQueryStringToJson(this);
+}
