2cbe078b2eed6740eac7a4fbed47be1a74543c07
youloveli <mymhj122@gmail.com>
2025-03-11 01:07:29 +0900

    :sparkles: :: WebSocketDataSourcesImpl, WebSocketDataSources 추가

---
+++ lib/data/data_sources/ws/web_socket_data_sources_impl.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:gogo_app/data/data_sources/ws/web_socket_data_sources.dart';
+import 'package:web_socket_channel/status.dart' as status;
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class WebSocketDataSourcesImpl implements WebSocketDataSources {
+  late final WebSocketChannel _channel;
+
+  WebSocketDataSourcesImpl(String url) {
+    _channel = WebSocketChannel.connect(Uri.parse(url));
+  }
+
+  @override
+  void sendMessage(Map<String, dynamic> message) {
+    final jsonString = jsonEncode(message);
+    _channel.sink.add(jsonString);
+  }
+
+  @override
+  Stream<Map<String, dynamic>> get messages => _channel.stream.map((event) {
+        return jsonDecode(event) as Map<String, dynamic>;
+      });
+
+  @override
+  void close() {
+    _channel.sink.close(status.normalClosure);
+  }
+}
