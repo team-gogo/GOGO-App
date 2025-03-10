cebf4be03c7fb56c0f047a122861119e57bc0322
youloveli <mymhj122@gmail.com>
2025-03-10 16:18:33 +0900

    :bug: :: AuthRepositoryImpl와 AuthDatasource 의 파일명이 바뀌어있어서 맞게 수정

--- lib/data/repositories/auth/auth_repository.dart
+++ lib/data/repositories/auth/auth_repository.dart
@@ -1,17 +1,7 @@
-import 'package:gogo_app/data/data_sources/auth/auth_data_source.dart';
-
 import 'package:gogo_app/data/models/auth/google_oauth/google_oauth_login_request.dart';
 import 'package:gogo_app/data/models/auth/google_oauth/google_oauth_login_response.dart';
-import 'auth_repository_impl.dart';
 
-class AuthRepositoryImpl implements AuthRepository {
-  final AuthDatasource _authDatasource;
-
-  AuthRepositoryImpl(this._authDatasource);
-
-  @override
+abstract class AuthRepository {
   Future<GoogleOAuthLoginResponse> googleOAuthLogin(
-      GoogleOAuthLoginRequest body) async {
-    return await _authDatasource.googleOAuthLogin(body);
-  }
+      GoogleOAuthLoginRequest body);
 }
--- lib/data/repositories/auth/auth_repository_impl.dart
+++ lib/data/repositories/auth/auth_repository_impl.dart
@@ -1,6 +1,17 @@
 import 'package:gogo_app/data/models/auth/google_oauth/google_oauth_login_request.dart';
 import 'package:gogo_app/data/models/auth/google_oauth/google_oauth_login_response.dart';
 
-abstract class AuthRepository {
-  Future<GoogleOAuthLoginResponse> googleOAuthLogin(GoogleOAuthLoginRequest body);
+import '../../data_sources/auth/auth_data_source.dart';
+import 'auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthDatasource _authDatasource;
+
+  AuthRepositoryImpl(this._authDatasource);
+
+  @override
+  Future<GoogleOAuthLoginResponse> googleOAuthLogin(
+      GoogleOAuthLoginRequest body) async {
+    return await _authDatasource.googleOAuthLogin(body);
+  }
 }
