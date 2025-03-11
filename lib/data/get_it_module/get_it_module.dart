48411a3e8159a8b10943b271fd48adada05a8708
answad <mymhj122@gmail.com>
2025-03-11 16:26:17 +0900

    :sparkles: :: MiniGameDataSource, MiniGameRepository get it 모듈에 등록

--- lib/data/get_it_module/get_it_module.dart
+++ lib/data/get_it_module/get_it_module.dart
@@ -4,6 +4,8 @@ import 'package:gogo_app/data/api/auth/auth_api.dart';
 import 'package:gogo_app/data/api/search_school/search_school_api.dart';
 import 'package:gogo_app/data/data_sources/auth/auth_data_source.dart';
 import 'package:gogo_app/data/data_sources/auth/auth_data_source_impl.dart';
+import 'package:gogo_app/data/data_sources/mini_game/mini_game_data_source.dart';
+import 'package:gogo_app/data/data_sources/mini_game/mini_game_data_source_impl.dart';
 import 'package:gogo_app/data/data_sources/search_school/search_school_data_source.dart';
 import 'package:gogo_app/data/data_sources/search_school/search_school_data_source_impl.dart';
 import 'package:gogo_app/data/data_sources/stage/stage_data_source.dart';
@@ -30,6 +32,8 @@ import '../data_sources/ws/web_socket_data_sources_impl.dart';
 import '../repositories/stage/stage_repository.dart';
 import '../repositories/stage/stage_repository_impl.dart';
 
+import '../repositories/mini_game/mini_game_repository.dart';
+import '../repositories/mini_game/mini_game_repository_impl.dart';
 import '../repositories/mini_game/ws/coin_toss/coin_toss_web_socket_repository_impl.dart';
 import '../repositories/mini_game/ws/plinko/plinko_web_socket_repository_impl.dart';
 import '../repositories/mini_game/ws/yavarwee/yavarwee_web_socket_repository_impl.dart';
@@ -45,6 +49,10 @@ void setupDataSourceLocator() {
       () => AuthDatasourceImpl(locator<Dio>()));
   locator.registerLazySingleton<StageDataSource>(
       () => StageDataSourceImpl(locator<Dio>()));
+  locator.registerLazySingleton<MiniGameDataSource>(
+      () => MiniGameDataSourceImpl(locator<Dio>()));
+  locator.registerLazySingleton<SearchSchoolDataSource>(
+      () => SearchSchoolDataSourceImpl(locator<Dio>()));
   locator.registerLazySingleton<SearchSchoolDataSource>(
       () => SearchSchoolDataSourceImpl(locator<Dio>()));
 
@@ -66,8 +74,9 @@ void setupRepositoryLocator() {
       () => StageRepositoryImpl(locator<StageDataSource>()));
   locator.registerLazySingleton<SearchSchoolRepository>(
       () => SearchSchoolRepositoryImpl(locator<SearchSchoolDataSource>()));
-  locator.registerLazySingleton<SearchSchoolDataSource>(
-      () => SearchSchoolDataSourceImpl(locator<Dio>()));
+  locator.registerLazySingleton<MiniGameRepository>(
+      () => MiniGameRepositoryImpl(locator<MiniGameDataSource>()));
+
   locator.registerLazySingleton<CoinTossWebSocketRepository>(
       () => CoinTossWebSocketRepositoryImpl(locator<WebSocketDataSource>()));
   locator.registerLazySingleton<PlinkoWebSocketRepository>(
