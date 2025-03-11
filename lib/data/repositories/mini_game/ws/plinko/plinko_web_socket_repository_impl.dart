45c7856e08ee78449bedf3928241df0119e59762
answad <mymhj122@gmail.com>
2025-03-11 14:01:31 +0900

    :sparkles: :: WebSocketDataSource의 이름변경 적용

--- lib/data/repositories/mini_game/ws/plinko/plinko_web_socket_repository_impl.dart
+++ lib/data/repositories/mini_game/ws/plinko/plinko_web_socket_repository_impl.dart
@@ -1,27 +1,27 @@
 import 'package:gogo_app/data/repositories/mini_game/ws/plinko/plinko_web_socket_repository.dart';
 
-import '../../../../data_sources/ws/web_socket_data_sources.dart';
+import '../../../../data_sources/ws/web_socket_data_source.dart';
 import '../../../../models/mini_game/ws/plinko_response.dart';
 
 class PlinkoWebSocketRepositoryImpl implements PlinkoWebSocketRepository {
-  final WebSocketDataSources _webSocketDataSources;
+  final WebSocketDataSource _webSocketDataSource;
 
-  PlinkoWebSocketRepositoryImpl(this._webSocketDataSources);
+  PlinkoWebSocketRepositoryImpl(this._webSocketDataSource);
 
   @override
   void sendBet(int amount, String risk) {
     final request = {"amount": amount, "risk": risk};
-    _webSocketDataSources.sendMessage(request);
+    _webSocketDataSource.sendMessage(request);
   }
 
   @override
   Stream<PlinkoResponse> listenForResponses() {
-    return _webSocketDataSources.messages
+    return _webSocketDataSource.messages
         .map((event) => PlinkoResponse.fromJson(event));
   }
 
   @override
   void closeConnection() {
-    _webSocketDataSources.close();
+    _webSocketDataSource.close();
   }
 }
