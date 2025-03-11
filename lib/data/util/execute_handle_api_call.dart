40a7d300bd7ae7585889fb8acee0486997881a39
parkyuhyeon <pyh1111802@naver.com>
2025-03-11 14:03:15 +0900

    :memo: :: Error 문구 변경

--- lib/data/util/execute_handle_api_call.dart
+++ lib/data/util/execute_handle_api_call.dart
@@ -1,13 +1,14 @@
- import 'package:dio/dio.dart';
+import 'package:dio/dio.dart';
 
 import 'handle_dio_error.dart';
 
 Future<T> executeHandleApiCall<T>(Future<T> Function() apiCall) async {
-    try {
-      return await apiCall();
-    } on DioException catch (e) {
-      throw handleDioError(e);
-    } catch (e) {
-      throw Exception('로그인 실패: $e');
-    }
+  try {
+    print("1234${await T}");
+    return await apiCall();
+  } on DioException catch (e) {
+    throw handleDioError(e);
+  } catch (e) {
+    throw Exception('실패: $e');
   }
+}
