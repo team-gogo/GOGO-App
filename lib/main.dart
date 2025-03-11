a3d7e8a3537663c2c74861de907eb84cbad4c506
parkyuhyeon <pyh1111802@naver.com>
2025-03-11 14:05:49 +0900

    :sparkles: :: Env파일 불러오는 코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:gogo_app/router.dart';
 import 'data/get_it_module/get_it_module.dart';
 import 'design_system/theme/color.dart';
@@ -7,6 +8,10 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // env 불러오기
+  await dotenv.load(fileName: ".env");
+  print(dotenv.env['NEIS_KEY']);
 
   // 파이어베이스 초기화
   await Firebase.initializeApp(
