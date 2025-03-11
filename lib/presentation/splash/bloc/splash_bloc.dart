5c1203fa86f112aaaf92db9c5c7171c9d0bc9b72
parkyuhyeon <pyh1111802@naver.com>
2025-03-11 21:14:29 +0900

    :sparkles: :: FCM을 위한 DeviceToken발급

--- lib/presentation/splash/bloc/splash_bloc.dart
+++ lib/presentation/splash/bloc/splash_bloc.dart
@@ -1,15 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:gogo_app/presentation/splash/bloc/splash_event.dart';
 import 'package:gogo_app/presentation/splash/bloc/splash_state.dart';
 import 'package:gogo_app/router.dart';
 
 class SplashBloc extends Bloc<SplashEvent, SplashState> {
+  late String deviceToken;
+
   SplashBloc() : super(InitSplashState()) {
     on<LaunchSplashEvent>((event, emit) async {
       emit(LoadingSplashState());
-      /// 초기 로그인 로직
+      deviceToken = await FirebaseMessaging.instance.getToken() ?? "";
       await Future.delayed(Duration(seconds: 2));
       emit(DisposeSplashState());
     });
   }
 }
+
