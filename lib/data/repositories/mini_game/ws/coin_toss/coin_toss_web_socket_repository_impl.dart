4ad8c0aa83c4adcbe2c544975cc690a20ca9fe99
answad <mymhj122@gmail.com>
2025-03-11 14:44:16 +0900

    :sparkles: :: CoinTossWebSocketRepositoryImpl, CoinTossWebSocketRepository 추가

---
+++ lib/data/repositories/mini_game/ws/coin_toss/coin_toss_web_socket_repository_impl.dart
@@ -0,0 +1,32 @@
+import '../../../../data_sources/ws/web_socket_data_source.dart';
+import '../../../../models/mini_game/ws/coin_toss_response.dart';
+import 'coin_toss_web_socket_repository.dart';
+
+class CoinTossWebSocketRepositoryImpl implements CoinTossWebSocketRepository {
+  final WebSocketDataSource _webSocketDataSource;
+
+  CoinTossWebSocketRepositoryImpl(this._webSocketDataSource);
+
+  @override
+  void connect(String stageId) {
+    _webSocketDataSource
+        .connect("wss://echo.websocket.org" + "/coin-toss/$stageId");
+  }
+
+  @override
+  void sendAmount(int amount) {
+    final request = {"amount": amount};
+    _webSocketDataSource.sendMessage(request);
+  }
+
+  @override
+  Stream<CoinTossResponse> listenForResponses() {
+    return _webSocketDataSource.messages
+        .map((event) => CoinTossResponse.fromJson(event));
+  }
+
+  @override
+  void closeConnection() {
+    _webSocketDataSource.close();
+  }
+}
