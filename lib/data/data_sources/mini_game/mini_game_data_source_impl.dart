49cb69ed14035ed48dd8c922ec8686dbf9b50776
answad <mymhj122@gmail.com>
2025-03-11 16:23:16 +0900

    :sparkles: :: MiniGameDataSource, MiniGameDataSourceImpl 구현

---
+++ lib/data/data_sources/mini_game/mini_game_data_source_impl.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+
+import '../../api/mini_game_api.dart';
+import '../../models/mini_game/active_game_response.dart';
+import '../../models/mini_game/ticket_counts_response.dart';
+import '../../util/execute_handle_api_call.dart';
+import 'mini_game_data_source.dart';
+
+class MiniGameDataSourceImpl implements MiniGameDataSource {
+  final MiniGameApi _miniGameApi;
+
+  MiniGameDataSourceImpl(Dio dio) : _miniGameApi = MiniGameApi(dio);
+
+  @override
+  Future<TicketCountsResponse> getTicketCount(String stageId) async {
+    return await executeHandleApiCall(
+        () => _miniGameApi.getTicketCount(stageId));
+  }
+
+  @override
+  Future<ActiveGameResponse> getActiveGame(String stageId) async {
+    return await executeHandleApiCall(
+        () => _miniGameApi.getActiveGame(stageId));
+  }
+}
