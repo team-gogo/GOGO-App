89400c69f5a46d45296d1bb15a438041c41b9652
명훈 <s23065@gsm.hs.kr>
2025-03-12 02:59:08 +0900

    :sparkles: :: Create ShopRepository(Impl)

---
+++ lib/data/repositories/shop/shop_repository.dart
@@ -0,0 +1,9 @@
+import '../../models/shop/request/buy_shop_item_request.dart';
+import '../../models/shop/response/shop_receipt_response.dart';
+import '../../models/shop/response/shop_ticket_status_response.dart';
+
+abstract class ShopRepository {
+  Future<ShopTicketStatusResponse> shopTicketStatusResponse (int stageId);
+  Future<ReceiptItem> getReceiptItem(int shopId);
+  Future<void> buyShopItemRequest(int shopId, BuyShopItemRequest body);
+}
