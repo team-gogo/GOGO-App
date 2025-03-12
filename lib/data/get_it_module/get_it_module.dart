cf9cb2058a8ccb9ee8856684e4a7012aca24ba5b
parkyuhyeon <pyh1111802@naver.com>
2025-03-12 20:13:47 +0900

    :memo: :: getit 충돌 해결

--- lib/data/get_it_module/get_it_module.dart
+++ lib/data/get_it_module/get_it_module.dart
@@ -18,20 +18,12 @@ import 'package:gogo_app/data/repositories/mini_game/ws/plinko/plinko_web_socket
 import 'package:gogo_app/data/repositories/mini_game/ws/yavarwee/yavarwee_web_socket_repository.dart';
 import 'package:gogo_app/data/repositories/search_school/search_school_repository.dart';
 import 'package:gogo_app/data/repositories/search_school/search_school_repository_impl.dart';
-
 import 'package:gogo_app/data/api/mini_game_api.dart';
 import 'package:gogo_app/data/api/stage/stage_api.dart';
 import 'package:gogo_app/data/data_sources/ws/web_socket_data_source.dart';
 import 'package:gogo_app/data/data_sources/ws/web_socket_data_sources_impl.dart';
 import 'package:gogo_app/data/repositories/stage/stage_repository.dart';
 import 'package:gogo_app/data/repositories/stage/stage_repository_impl.dart';
-import '../api/mini_game_api.dart';
-import '../api/stage_api.dart';
-import '../data_sources/ws/web_socket_data_source.dart';
-import '../data_sources/ws/web_socket_data_sources_impl.dart';
-import '../repositories/stage/stage_repository.dart';
-import '../repositories/stage/stage_repository_impl.dart';
-
 import '../repositories/mini_game/mini_game_repository.dart';
 import '../repositories/mini_game/mini_game_repository_impl.dart';
 import '../repositories/mini_game/ws/coin_toss/coin_toss_web_socket_repository_impl.dart';
@@ -46,47 +38,39 @@ void setUpDio() {
 
 void setupDataSourceLocator() {
   locator.registerLazySingleton<AuthDatasource>(
-      () => AuthDatasourceImpl(locator<Dio>()));
+          () => AuthDatasourceImpl(locator<Dio>()));
   locator.registerLazySingleton<StageDataSource>(
-      () => StageDataSourceImpl(locator<Dio>()));
+          () => StageDataSourceImpl(locator<Dio>()));
   locator.registerLazySingleton<MiniGameDataSource>(
-      () => MiniGameDataSourceImpl(locator<Dio>()));
+          () => MiniGameDataSourceImpl(locator<Dio>()));
   locator.registerLazySingleton<SearchSchoolDataSource>(
-      () => SearchSchoolDataSourceImpl(locator<Dio>()));
-  locator.registerLazySingleton<SearchSchoolDataSource>(
-      () => SearchSchoolDataSourceImpl(locator<Dio>()));
+          () => SearchSchoolDataSourceImpl(locator<Dio>()));
 
-  locator.registerFactory<WebSocketDataSource>(() =>
-      WebSocketDataSourceImpl());
-}
-
-void setupApiLocator() {
-  locator.registerLazySingleton<AuthApi>(() => AuthApi(locator<Dio>()));
-  locator.registerLazySingleton<StageApi>(() => StageApi(locator<Dio>()));
-  locator.registerLazySingleton<SearchSchoolApi>(
-      () => SearchSchoolApi(locator<Dio>()));
+  locator.registerFactory<WebSocketDataSource>(() => WebSocketDataSourceImpl());
 }
 
 void setupRepositoryLocator() {
   locator.registerLazySingleton<AuthRepository>(
-      () => AuthRepositoryImpl(locator<AuthDatasource>()));
+          () => AuthRepositoryImpl(locator<AuthDatasource>()));
   locator.registerLazySingleton<StageRepository>(
-      () => StageRepositoryImpl(locator<StageDataSource>()));
+          () => StageRepositoryImpl(locator<StageDataSource>()));
   locator.registerLazySingleton<SearchSchoolRepository>(
-      () => SearchSchoolRepositoryImpl(locator<SearchSchoolDataSource>()));
+          () => SearchSchoolRepositoryImpl(locator<SearchSchoolDataSource>()));
   locator.registerLazySingleton<MiniGameRepository>(
-      () => MiniGameRepositoryImpl(locator<MiniGameDataSource>()));
+          () => MiniGameRepositoryImpl(locator<MiniGameDataSource>()));
 
   locator.registerLazySingleton<CoinTossWebSocketRepository>(
-      () => CoinTossWebSocketRepositoryImpl(locator<WebSocketDataSource>()));
+          () => CoinTossWebSocketRepositoryImpl(locator<WebSocketDataSource>()));
   locator.registerLazySingleton<PlinkoWebSocketRepository>(
-      () => PlinkoWebSocketRepositoryImpl(locator<WebSocketDataSource>()));
+          () => PlinkoWebSocketRepositoryImpl(locator<WebSocketDataSource>()));
   locator.registerLazySingleton<YavarweeWebSocketRepository>(
-      () => YavarweeWebSocketRepositoryImpl(locator<WebSocketDataSource>()));
+          () => YavarweeWebSocketRepositoryImpl(locator<WebSocketDataSource>()));
 }
 
 void setupApiLocator() {
   locator.registerLazySingleton<AuthApi>(() => AuthApi(locator<Dio>()));
   locator.registerLazySingleton<StageApi>(() => StageApi(locator<Dio>()));
   locator.registerLazySingleton<MiniGameApi>(() => MiniGameApi(locator<Dio>()));
+  locator.registerLazySingleton<SearchSchoolApi>(
+          () => SearchSchoolApi(locator<Dio>()));
 }
