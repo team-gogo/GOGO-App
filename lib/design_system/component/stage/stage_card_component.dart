cd51014c8e3ead613fd98edfa045f6849fcec2b8
LeeJueon <loolloo080313@gmail.com>
2025-03-12 15:09:34 +0900

    �::Minigame_play_component ui 완성

--- lib/design_system/component/stage/stage_card_component.dart
+++ lib/design_system/component/stage/stage_card_component.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gogo_app/design_system/component/button/gogo_icon_button.dart';
 import 'package:gogo_app/design_system/component/tag/tag_component.dart';
 import 'package:gogo_app/design_system/theme/icon.dart';
@@ -9,29 +8,49 @@ import '../../theme/typography.dart';
 class StageCardComponent extends StatelessWidget {
   final String stageName;
   final TextStyle stageTextStyle;
+  final double height;
+  final double width;
   final Color color;
+  final BorderRadius borderRadius;
+  final EdgeInsets tagPadding;
+  final double tagSpacing;
+  final double tagInsideSpacing;
   final TextStyle tagTextStyle;
+  final BorderRadius tagBorderRadius;
+  final double iconSize;
+  final EdgeInsets padding;
   final bool official; // 공식: true, 비공식: false
   final bool recruiting; // 모집 중: true, 모집 확정: false
   final bool manager; // 관리자 태그가 보임: true, 안보임: false
   final bool broadcast; // 중계 설정 태그가 보임: true, 안보임: false
   final VoidCallback onTap;
   final String buttonText;
+  final double buttonHeight;
   final double buttonWidth;
   final Widget buttonIcon;
 
   const StageCardComponent({
     super.key,
+    this.height = 220,
+    this.width = double.infinity,
     required this.stageName,
     this.stageTextStyle = GogoTypography.body1Extrabold,
     required this.color,
+    this.borderRadius = const BorderRadius.all(Radius.circular(16)),
+    this.tagPadding = const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+    this.tagSpacing = 12,
+    this.tagInsideSpacing = 6,
     this.tagTextStyle = GogoTypography.caption3Semibold,
+    this.tagBorderRadius = const BorderRadius.all(Radius.circular(8)),
+    this.iconSize = 12,
+    this.padding = const EdgeInsets.all(16),
     required this.official,
     required this.recruiting,
     required this.manager,
     required this.broadcast,
     required this.onTap,
     required this.buttonText,
+    this.buttonHeight = 60,
     this.buttonWidth = double.infinity,
     required this.buttonIcon,
   });
@@ -39,115 +58,108 @@ class StageCardComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(right: 16.w),
+      padding: padding,
       child: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(16.r)),
+          borderRadius: borderRadius,
           color: color,
         ),
-        height: 183.h,
-        width: 320.w,
+        height: height,
+        width: width,
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 14.h),
+          padding: padding,
           child: Stack(
             children: [
               Align(
                 alignment: Alignment.topLeft,
                 child: Row(
-                  spacing: 8.w,
                   children: [
                     official
-                        ? TagComponent.small(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 8.w, vertical: 4.h),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(8.h)),
-                            spacing: 4.w,
+                        ? TagComponent(
+                            padding: tagPadding,
+                            borderRadius: tagBorderRadius,
+                            spacing: tagInsideSpacing,
                             color: GogoColors.white,
                             text: '공식',
                             textStyle: tagTextStyle,
                             icon: GogoIcons.trophy(
-                              width: 12.sp,
-                              height: 12.sp,
+                              width: iconSize,
+                              height: iconSize,
                               color: GogoColors.white,
                             ),
                           )
-                        : SizedBox.shrink(),
-                    TagComponent.small(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 8.w, vertical: 4.h),
-                      borderRadius: BorderRadius.all(Radius.circular(8.h)),
-                      spacing: 4.w,
+                        : Container(),
+                    SizedBox(
+                      width: tagSpacing,
+                    ),
+                    TagComponent(
+                      padding: tagPadding,
+                      borderRadius: tagBorderRadius,
+                      spacing: tagInsideSpacing,
                       color:
                           recruiting ? GogoColors.success : GogoColors.gray500,
                       text: recruiting ? '모집 중' : '모집 확정',
                       textStyle: tagTextStyle,
                       icon: GogoIcons.stage(
-                        width: 12.sp,
-                        height: 12.sp,
+                        width: iconSize,
+                        height: iconSize,
                         color: recruiting
                             ? GogoColors.success
                             : GogoColors.gray500,
                       ),
                     ),
+                    SizedBox(
+                      width: tagSpacing,
+                    ),
                     manager
-                        ? TagComponent.small(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 8.w, vertical: 4.h),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(8.h)),
-                            spacing: 4.w,
+                        ? TagComponent(
+                            padding: tagPadding,
+                            borderRadius: tagBorderRadius,
+                            spacing: tagInsideSpacing,
                             color: GogoColors.main500,
                             text: '관리자',
                             textStyle: tagTextStyle,
                             icon: GogoIcons.person(
-                              width: 12.sp,
-                              height: 12.sp,
+                              width: iconSize,
+                              height: iconSize,
                               color: GogoColors.main500,
                             ),
                           )
-                        : SizedBox.shrink(),
-                    SizedBox(width: 16.w),
+                        : Container(),
                   ],
                 ),
               ),
               Align(
                 alignment: Alignment.topRight,
                 child: broadcast
-                    ? TagComponent.small(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 8.w, vertical: 4.h),
-                        borderRadius: BorderRadius.all(Radius.circular(8.h)),
-                        spacing: 4.w,
+                    ? TagComponent(
+                        padding: tagPadding,
+                        borderRadius: tagBorderRadius,
+                        spacing: tagInsideSpacing,
                         color: GogoColors.error,
                         text: '중계 설정',
                         textStyle: tagTextStyle,
                         icon: GogoIcons.play(
-                          width: 12.sp,
-                          height: 12.sp,
+                          width: iconSize,
+                          height: iconSize,
                           color: GogoColors.error,
                         ),
                       )
-                    : SizedBox.shrink(),
+                    : Container(),
               ),
               Align(
                 alignment: Alignment.center,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 28.h),
-                  child: Text(
-                    stageName,
-                    style: stageTextStyle.copyWith(color: GogoColors.white),
-                  ),
+                child: Text(
+                  stageName,
+                  style: stageTextStyle.copyWith(color: GogoColors.white),
                 ),
               ),
               Align(
                 alignment: Alignment.bottomCenter,
                 child: SizedBox(
-                  height: 45.h,
+                  height: buttonHeight,
                   child: GogoIconButton(
-                    padding: EdgeInsets.symmetric(horizontal: 16.sp),
                     icon: buttonIcon,
-                    spacing: 8.w,
                     onTap: onTap,
                     text: buttonText,
                     width: buttonWidth,
