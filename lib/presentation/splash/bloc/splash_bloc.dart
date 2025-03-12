26ce0217c3e0076e377b245e393f597c2740b06f
parkyuhyeon <pyh1111802@naver.com>
2025-03-12 20:02:01 +0900

    :sparkles: :: FCM 세팅 (중간커밋)

--- lib/presentation/splash/bloc/splash_bloc.dart
+++ lib/presentation/splash/bloc/splash_bloc.dart
@@ -1,8 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:gogo_app/presentation/splash/bloc/splash_event.dart';
 import 'package:gogo_app/presentation/splash/bloc/splash_state.dart';
-import 'package:gogo_app/router.dart';
 
 class SplashBloc extends Bloc<SplashEvent, SplashState> {
   late String deviceToken;
@@ -10,10 +11,28 @@ class SplashBloc extends Bloc<SplashEvent, SplashState> {
   SplashBloc() : super(InitSplashState()) {
     on<LaunchSplashEvent>((event, emit) async {
       emit(LoadingSplashState());
+      _settingPermission();
       deviceToken = await FirebaseMessaging.instance.getToken() ?? "";
+      print(deviceToken);
       await Future.delayed(Duration(seconds: 2));
       emit(DisposeSplashState());
     });
   }
-}
 
+  void _settingPermission() async {
+    ///================================================================= 알림 권한 설정
+    await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      announcement: true,
+      badge: true,
+      carPlay: true,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    ///================================================================= 포그라운드 알림 처리
+    FirebaseMessaging.onMessage
+        .listen((RemoteMessage message) => debugPrint(message.data.toString()));
+  }
+}
