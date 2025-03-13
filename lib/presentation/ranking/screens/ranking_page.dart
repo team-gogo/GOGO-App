331fdda93b3d2f23ca9661275ed3e422532bccd4
sysop <s24022@gmail.com>
2025-03-13 20:43:41 +0900

    💄:: Ranking 페이지 퍼블리싱

--- lib/presentation/ranking/screens/ranking_page.dart
+++ lib/presentation/ranking/screens/ranking_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gogo_app/design_system/component/top_bar/gogo_top_bar.dart';
 import 'package:gogo_app/design_system/theme/color.dart';
+import 'package:gogo_app/design_system/theme/typography.dart';
+import 'package:gogo_app/presentation/ranking/widgets/ranking_component.dart';
 import 'package:gogo_app/presentation/ranking/widgets/ranking_list_item.dart';
 
 class RankingPage extends StatelessWidget {
@@ -27,8 +29,55 @@ class RankingPage extends StatelessWidget {
                     Radius.circular(12),
                   ),
                 ),
-                child: SizedBox(
-                  width: 302,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'TOP 3',
+                      style: GogoTypography.body2Extrabold
+                          .copyWith(color: Colors.white),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        RankingComponent(
+                          points: '1500',
+                          name: '나현욱',
+                          colors: [
+                            Color(0xFF989898),
+                            Color(0xFFD5D5D5),
+                            Color(0xFF676767),
+                          ],
+                          circleSize: 70,
+                          iconSize: 24,
+                        ),
+                        RankingComponent(
+                          points: '2000',
+                          name: '나현욱',
+                          colors: [
+                            Color(0xFFA07102),
+                            Color(0xFFFADC73),
+                            Color(0xFF9F812E),
+                          ],
+                          circleSize: 90,
+                          iconSize: 40,
+                        ),
+                        RankingComponent(
+                          points: '1000',
+                          name: '나현욱',
+                          colors: [
+                            Color(0xFFAE5C43),
+                            Color(0xFFF4A98C),
+                            Color(0xFF763920),
+                          ],
+                          circleSize: 70,
+                          iconSize: 24,
+                        )
+                      ],
+                    ),
+                  ],
                 ),
               ),
               Expanded(
@@ -48,7 +97,8 @@ class RankingPage extends StatelessWidget {
                         20,
                         (index) => SizedBox(
                           height: 38,
-                          child: RankingListItem(index: index, name: '김진원', point: 123),
+                          child: RankingListItem(
+                              index: index, name: '김진원', point: 123),
                         ),
                       ),
                     ),
