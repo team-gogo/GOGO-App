e3c75acdd99be3917fa428042a02c7c30ad7d0e4
answad <mymhj122@gmail.com>
2025-03-13 20:06:22 +0900

    :lipstick: :: StageBaseComponent 퍼블리싱

---
+++ lib/presentation/stage_create/widgets/stage_base_component.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:gogo_app/design_system/theme/color.dart';
+import 'package:gogo_app/design_system/theme/typography.dart';
+
+class StageBaseComponent extends StatelessWidget {
+  final String title;
+  final String? description;
+  final List<Widget> children;
+
+  const StageBaseComponent({
+    super.key,
+    required this.title,
+    this.description,
+    required this.children,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: GogoColors.black,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children: [
+              Text(
+                title,
+                style: GogoTypography.body2Extrabold
+                    .copyWith(color: GogoColors.white),
+              ),
+              const SizedBox(width: 12),
+              description != null
+                  ? Text(
+                      description!,
+                      style: GogoTypography.caption2Semibold,
+                    )
+                  : SizedBox.shrink()
+            ],
+          ),
+          Column(
+            spacing: 12,
+            children: [
+              ...children,
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
