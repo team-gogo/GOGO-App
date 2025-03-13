8896964d90e59b9a6938b92a854d9410898b8619
parkyuhyeon <pyh1111802@naver.com>
2025-03-13 19:36:23 +0900

    :lipstick: :: Ranking List Item 컴포넌트 퍼블리싱

---
+++ lib/presentation/ranking/widgets/ranking_list_item.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:gogo_app/design_system/theme/color.dart';
+import 'package:gogo_app/design_system/theme/typography.dart';
+
+class RankingListItem extends StatelessWidget {
+  final int index;
+  final String name;
+  final double point;
+
+  const RankingListItem(
+      {super.key,
+      required this.index,
+      required this.name,
+      required this.point});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: BoxDecoration(
+        color: GogoColors.gray600,
+        borderRadius: BorderRadius.all(
+          Radius.circular(8),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            spacing: 24,
+            children: [
+              Text(
+                "$index등",
+                style: GogoTypography.caption1Extrabold.copyWith(
+                  color: GogoColors.white,
+                ),
+              ),
+              Text(
+                name,
+                style: GogoTypography.caption1Semibold.copyWith(
+                  color: GogoColors.gray300,
+                ),
+              ),
+            ],
+          ),
+          Text(
+            "${point.toInt()}P",
+            style: GogoTypography.caption1Extrabold.copyWith(
+              color: GogoColors.main400,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
