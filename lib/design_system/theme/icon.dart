21301cde035a8e9cc271a69613a5b406b1c21529
youloveli <mymhj122@gmail.com>
2025-03-13 23:35:52 +0900

    :sparkles: :: pointCoin 추가

--- lib/design_system/theme/icon.dart
+++ lib/design_system/theme/icon.dart
@@ -540,4 +540,13 @@ class GogoIcons {
   }) =>
       _buildIcon('point_circle.svg',
           color: color, width: width, height: height, onTap: onTap);
+
+  static Widget pointCoin({
+    Color? color,
+    double width = 24,
+    double height = 24,
+    VoidCallback? onTap,
+  }) =>
+      _buildIcon('point_coin.svg',
+          color: color, width: width, height: height, onTap: onTap);
 }
