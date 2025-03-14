d71779a626958d7ce678f52e8cd77c3d01437d5f
parkyuhyeon <pyh1111802@naver.com>
2025-03-14 16:10:30 +0900

    :lipstick: :: EditProfileScreen 퍼블리싱

--- lib/presentation/profile/widget/page/edit_profile_screen.dart
+++ lib/presentation/profile/widget/page/edit_profile_screen.dart
@@ -1,12 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:gogo_app/data/models/auth/additional_sign_up/additional_sign_up_response.dart';
+import 'package:gogo_app/design_system/component/text_field/gogo_text_field.dart';
+import 'package:gogo_app/design_system/component/top_bar/gogo_top_bar.dart';
+import 'package:gogo_app/design_system/theme/color.dart';
+import 'package:gogo_app/design_system/theme/icon.dart';
+import 'package:gogo_app/design_system/theme/typography.dart';
 
-class EditProfilePage extends StatelessWidget {
-  const EditProfilePage({super.key});
+import '../../../../design_system/component/button/gogo_default_button.dart';
+
+class EditProfilePage extends StatefulWidget {
+  EditProfilePage({super.key});
+
+  @override
+  State<EditProfilePage> createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  final TextEditingController _nameController = TextEditingController();
+
+  final TextEditingController _gradeController = TextEditingController();
+
+  final TextEditingController _classController = TextEditingController();
+
+  final TextEditingController _numberController = TextEditingController();
+
+  Sex _sexController = Sex.FEMALE;
+
+  bool profanityFilter = false;
+
+  Widget _editItem(String text, Widget child) => Column(
+        spacing: 16,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            text,
+            style: GogoTypography.body2Extrabold.copyWith(
+              color: GogoColors.white,
+            ),
+          ),
+          child
+        ],
+      );
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(),
+    return Scaffold(
+      backgroundColor: GogoColors.black,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(16, 17, 16, 60),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 36,
+              children: [
+                GogoTopBar(
+                  title: '정보수정',
+                  onBackTap: () {},
+                ),
+                _editItem(
+                    '이름',
+                    GogoTextField(
+                        controller: _nameController, hintText: '김진원')),
+                _editItem(
+                    '학년',
+                    GogoTextField(
+                        controller: _gradeController, hintText: '1학년')),
+                _editItem(
+                    '반',
+                    GogoTextField(
+                        controller: _classController, hintText: '1반')),
+                _editItem(
+                    '번호',
+                    GogoTextField(
+                        controller: _numberController, hintText: '1번')),
+                _editItem(
+                  '성별',
+                  Column(
+                    spacing: 12,
+                    children: [
+                      GogoDefaultButton(
+                        onTap: () => setState(() {
+                          _sexController = Sex.MALE;
+                        }),
+                        text: "남성",
+                        color: _sexController == Sex.MALE
+                            ? GogoColors.main600
+                            : GogoColors.gray400,
+                      ),
+                      GogoDefaultButton(
+                        onTap: () => setState(() {
+                          _sexController = Sex.FEMALE;
+                        }),
+                        text: "여성",
+                        color: _sexController == Sex.FEMALE
+                            ? GogoColors.main600
+                            : GogoColors.gray400,
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '비속어 필터',
+                      style: GogoTypography.body2Extrabold.copyWith(
+                        color: GogoColors.white,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () => setState(() {
+                        profanityFilter = !profanityFilter;
+                      }),
+                      child: profanityFilter
+                          ? GogoIcons.checkboxOutlined(
+                              width: 36,
+                              height: 36,
+                              color: GogoColors.main600,
+                            )
+                          : GogoIcons.checkboxOutlined(
+                              width: 36,
+                              height: 36,
+                              color: GogoColors.gray500,
+                            ),
+                    )
+                  ],
+                ),
+                GogoDefaultButton(onTap: () {}, text: '확인')
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
