a85c9d917078afddc0b781ab4e0e90e217d9266c
answad <mymhj122@gmail.com>
2025-03-14 19:25:29 +0900

    :lipstick: :: plinko component PlinkoGame 퍼블리싱, 구현

--- lib/presentation/mini_game/plinko/plinko.dart
+++ lib/presentation/mini_game/plinko/plinko.dart
@@ -1,11 +1,13 @@
 import 'dart:math';
-import 'dart:ui';
 
 import 'package:flame/components.dart';
-import 'package:flame_forge2d/body_component.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flame_forge2d/forge2d_game.dart';
 import 'package:gogo_app/design_system/theme/color.dart';
+
+import 'component/ball.dart';
+import 'component/obstacle.dart';
+import 'component/rounded_box.dart';
 
 double getRandomNumber(double min, double max) {
   final random = Random();
@@ -23,56 +25,29 @@ class PlinkoGame extends Forge2DGame {
   void addObstacles() async {
     double startX = size.x / 2;
     double startY = size.y / 5;
-    double spacing = 25;
+    double spacing = 22;
 
-    for (int row = 0; row < 15; row++) {
+    for (int row = 0; row < 16 + 2; row++) {
       for (int col = 0; col <= row; col++) {
-        if (row == 0 || row == 1) continue;
         double x = startX + col * spacing - (row * spacing / 2);
         double y = startY + row * spacing;
+        if (row == 0 || row == 1) continue;
+        if (row == 17 && col != 0) {
+          add(
+            RoundedBoxComponent(
+              color: GogoColors.main100,
+              width: 17,
+              height: 15,
+            )..position = Vector2(x - 20, y + 20),
+          );
+        }
+
         add(Obstacle(Vector2(x, y)));
       }
     }
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 10000; i++) {
       await Future.delayed(Duration(milliseconds: 1000));
       add(Ball(Vector2(startX + getRandomNumber(-10, 10), startY - 50)));
     }
-  }
-}
-
-class Obstacle extends BodyComponent {
-  final Vector2 position;
-  final Color color; // 색상 추가
-  late Paint _paint;
-
-  Obstacle(this.position, {this.color = GogoColors.main300}) {
-    _paint = Paint()..color = color;
-  }
-
-  @override
-  Body createBody() {
-    final shape = CircleShape()..radius = 4;
-    final fixtureDef = FixtureDef(shape, density: 1, restitution: 0.3);
-    final bodyDef = BodyDef(position: position, type: BodyType.static);
-    return world.createBody(bodyDef)..createFixture(fixtureDef);
-  }
-
-  @override
-  void renderCircle(Canvas canvas, Offset center, double radius) {
-    canvas.drawCircle(center, radius, _paint);
-  }
-}
-
-class Ball extends BodyComponent {
-  final Vector2 position;
-
-  Ball(this.position);
-
-  @override
-  Body createBody() {
-    final shape = CircleShape()..radius = 5;
-    final fixtureDef = FixtureDef(shape, density: 1, restitution: 0.9);
-    final bodyDef = BodyDef(position: position, type: BodyType.dynamic);
-    return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
 }
