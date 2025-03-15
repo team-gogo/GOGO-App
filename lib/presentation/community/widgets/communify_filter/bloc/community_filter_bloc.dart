12f4cb268be8a3012f67b288fc844f74c894d939
parkyuhyeon <pyh1111802@naver.com>
2025-03-15 13:08:56 +0900

    :sparkles: :: CommunityFilterPopup Bloc추가

---
+++ lib/presentation/community/widgets/communify_filter/bloc/community_filter_bloc.dart
@@ -0,0 +1,52 @@
+import 'package:bloc/bloc.dart';
+import 'package:gogo_app/presentation/community/widgets/communify_filter/bloc/community_filter_event.dart';
+import 'package:gogo_app/presentation/community/widgets/communify_filter/bloc/community_filter_state.dart';
+
+class CommunitySportFilterBloc
+    extends Bloc<CommunitySportFilterEvent, CommunitySportFilterState> {
+  CommunitySportFilterBloc() : super(DefaultCommunitySportFilterState()) {
+    on<SelectCommunitySportFilterEvent>(_handleSelectCommunitySportFilterEvent);
+  }
+
+  void _handleSelectCommunitySportFilterEvent(
+      SelectCommunitySportFilterEvent event,
+      Emitter<CommunitySportFilterState> emit) {
+    print(event.gameType);
+    if (state is SelectedCommunitySportFilterState) {
+      final currentState = state as SelectedCommunitySportFilterState;
+      print(currentState.gameType);
+      if (event.gameType == currentState.gameType) {
+        emit(DefaultCommunitySportFilterState());
+      } else {
+        emit(SelectedCommunitySportFilterState(gameType: event.gameType));
+      }
+    } else {
+      emit(SelectedCommunitySportFilterState(gameType: event.gameType));
+    }
+  }
+}
+
+
+class CommunitySortFilterBloc
+    extends Bloc<CommunitySortFilterEvent, CommunitySortFilterState> {
+  CommunitySortFilterBloc() : super(DefaultCommunitySortFilterState()) {
+    on<SelectCommunitySortFilterEvent>(_handleSelectCommunitySortFilterEvent);
+  }
+
+  void _handleSelectCommunitySortFilterEvent(
+      SelectCommunitySortFilterEvent event,
+      Emitter<CommunitySortFilterState> emit) {
+    print(event.sortType);
+    if (state is SelectedCommunitySortFilterState) {
+      final currentState = state as SelectedCommunitySortFilterState;
+      print(currentState.sortType);
+      if (event.sortType == currentState.sortType) {
+        emit(DefaultCommunitySortFilterState());
+      } else {
+        emit(SelectedCommunitySortFilterState(sortType: event.sortType));
+      }
+    } else {
+      emit(SelectedCommunitySortFilterState(sortType: event.sortType));
+    }
+  }
+}
