31df6cabc7cb2d3e8ea0a69e360054260c547e25
LeeJueon <loolloo080313@gmail.com>
2025-03-16 06:07:25 +0900

    💄:: Minigame 공통 컴포넌트 퍼블리싱싱

---
+++ lib/presentation/minigame/widgets/minigame_component.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gogo_app/design_system/component/text_field/gogo_text_field.dart';
+import 'package:gogo_app/design_system/theme/color.dart';
+import 'package:gogo_app/design_system/theme/icon.dart';
+import 'package:gogo_app/design_system/theme/typography.dart';
+
+class MinigameComponent extends StatelessWidget {
+  final double width;
+
+  const MinigameComponent({
+    this.width = 343,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController controller =
+        TextEditingController(); // 삭제 해야할 값
+
+    return SizedBox(
+      width: width.w,
+      height: 180,
+      child: Column(
+        spacing: 20,
+        children: [
+          SizedBox(
+            width: width.w,
+            height: 32.h,
+            child: Row(
+              spacing: 12.w,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "보유 코인",
+                  style: GogoTypography.body3Extrabold.copyWith(
+                    color: GogoColors.gray300,
+                  ),
+                ),
+                GogoIcons.stackedCoins(
+                  color: GogoColors.white,
+                ),
+                Text(
+                  '2000',
+                  style: GogoTypography.body2Semibold.copyWith(
+                    color: GogoColors.white,
+                  ),
+                ),
+                SizedBox(
+                  width: 12.w,
+                ),
+                Text(
+                  '티켓',
+                  style: GogoTypography.body3Semibold.copyWith(
+                    color: GogoColors.gray300,
+                  ),
+                ),
+                GogoIcons.ticket(
+                  color: GogoColors.white,
+                ),
+                Text(
+                  "2",
+                  style: GogoTypography.body3Semibold.copyWith(
+                    color: GogoColors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          GogoTextField(
+            controller: controller,
+            hintText: '포인트를 입력해주세요',
+            backgroundColor: GogoColors.gray700,
+            keyboardType: TextInputType.number,
+            endIcon: GogoIcons.pointCircle(),
+          ),
+          Container(
+            width: width.w,
+            height: 56,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: GogoColors.gray400,
+            ),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                minimumSize: Size.zero,
+              ),
+              onPressed: () {},
+              child: Text(
+                '뒷집기',
+                style: GogoTypography.body3Semibold.copyWith(
+                  color: GogoColors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
