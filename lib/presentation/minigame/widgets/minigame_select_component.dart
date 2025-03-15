44ac5461e0af1b561c1c8005d868d5d9329c0721
LeeJueon <loolloo080313@gmail.com>
2025-03-16 02:42:03 +0900

    ♻️:: 위젯 분리및 이름 변경

--- lib/presentation/minigame/widgets/minigame_select_component.dart
+++ lib/presentation/minigame/widgets/minigame_select_component.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gogo_app/design_system/theme/color.dart';
 import 'package:gogo_app/design_system/theme/icon.dart';
 import 'package:gogo_app/design_system/theme/typography.dart';
+import 'package:gogo_app/presentation/logIn/screen/login_screen.dart';
 
 class MinigameSelectComponent extends StatelessWidget {
   final double width;
@@ -91,5 +92,101 @@ class MinigameSelectComponent extends StatelessWidget {
         height: 12,
       ),
     ]);
+  }
+}
+
+class MinigameComponent extends StatelessWidget {
+  final Widget component;
+  final Widget icon;
+  final String text;
+  final int shellgameTicketscost; // 코인토스 티켓 가격
+  final int cointTossTicketscost; // 플링코 티켓 가격
+  final int plinkoTicketscost; // 야바위 티켓 가격
+  final int shellgameTicketsCount; // 야바위 티켓 구매 가능한 수수
+  final int cointTossTicketsCount; // 코인토스 티켓 구매 가능한 수
+  final int plinkoTicketsCount; // 야바위 티켓 구매 가능한 수
+  final bool info; // 티켓이면 true, 포인트 정보이면 false
+
+  const MinigameComponent({
+    super.key,
+    required this.component,
+    required this.icon,
+    required this.text,
+    this.shellgameTicketscost = 0,
+    this.cointTossTicketscost = 0,
+    this.plinkoTicketscost = 0,
+    this.shellgameTicketsCount = 0,
+    this.cointTossTicketsCount = 0,
+    this.plinkoTicketsCount = 0,
+    required this.info,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        MinigameTopBar(
+          height: 32,
+          component: component,
+          icon: icon,
+          text: text,
+        ),
+        SizedBox(
+          height: 12,
+        ),
+        MinigameSelectComponent(
+          info: info,
+          gameIcon: info
+              ? GogoIcons.shellGame(
+                  width: 48,
+                  height: 48,
+                  color: GogoColors.white,
+                )
+              : GogoIcons.ticket(
+                  width: 48,
+                  height: 48,
+                  color: GogoColors.white,
+                ),
+          gameName: "야바위",
+          ticketsCost: shellgameTicketscost,
+          ticketsCount: shellgameTicketsCount,
+        ),
+        MinigameSelectComponent(
+          info: info,
+          gameIcon: info
+              ? GogoIcons.pointCoin(
+                  width: 48,
+                  height: 48,
+                  color: GogoColors.white,
+                )
+              : GogoIcons.ticket(
+                  width: 48,
+                  height: 48,
+                  color: GogoColors.white,
+                ),
+          gameName: "코인토스",
+          ticketsCost: cointTossTicketscost,
+          ticketsCount: cointTossTicketsCount,
+        ),
+        MinigameSelectComponent(
+          info: info,
+          gameIcon: info
+              ? GogoIcons.plinko(
+                  width: 48,
+                  height: 48,
+                  color: GogoColors.white,
+                )
+              : GogoIcons.ticket(
+                  width: 48,
+                  height: 48,
+                  color: GogoColors.white,
+                ),
+          gameName: "플링코",
+          ticketsCost: plinkoTicketscost,
+          ticketsCount: plinkoTicketsCount,
+        ),
+      ],
+    );
   }
 }
