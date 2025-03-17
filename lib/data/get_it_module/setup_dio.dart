dbd7bde5a6d541de55848d955045fc858ebdb3e4
answad <mymhj122@gmail.com>
2025-03-17 13:35:53 +0900

    :recycle: :: LogInterceptor를 settingDio 에서 분리하고 별계의 클래스로 선언

--- lib/data/get_it_module/setup_dio.dart
+++ lib/data/get_it_module/setup_dio.dart
@@ -8,25 +8,7 @@ Dio settingDio() {
       receiveTimeout: Duration(seconds: 30),
     ),
   );
-
-  dio.interceptors.add(InterceptorsWrapper(
-    onRequest: (options, handler) {
-      print('Request: ${options.method} ${options.uri}');
-      print('Request Headers: ${options.headers}');
-      print('Request Data: ${options.data}');
-      return handler.next(options);
-    },
-    onResponse: (response, handler) {
-      print('Response: ${response.statusCode} ${response.requestOptions.uri}');
-      print('Response Data: ${response.data}');
-      return handler.next(response);
-    },
-    onError: (DioException e, handler) {
-      print('Error: ${e.response?.statusCode} ${e.requestOptions.uri}');
-      print('Error Message: ${e.message}');
-      return handler.next(e);
-    },
-  ));
+  dio.interceptors.add(LogInterceptor());
 
   return dio;
 }
