35fce38ef17f963f39615774de4d1dd406db87b8
answad <mymhj122@gmail.com>
2025-03-17 10:15:11 +0900

    :recycle: :: AuthRepository 에 additionalSignUp, tokenRefresh, logOut함수 추가

--- lib/data/repositories/auth/auth_repository.dart
+++ lib/data/repositories/auth/auth_repository.dart
@@ -1,7 +1,12 @@
 import 'package:gogo_app/data/models/auth/google_oauth/google_oauth_login_request.dart';
 import 'package:gogo_app/data/models/auth/google_oauth/google_oauth_login_response.dart';
 
+import '../../models/auth/additional_sign_up/additional_sign_up_response.dart';
+
 abstract class AuthRepository {
   Future<GoogleOAuthLoginResponse> googleOAuthLogin(
       GoogleOAuthLoginRequest body);
+  Future<void> additionalSignUp(AdditionalSignUpRequest body);
+  Future<void> tokenRefresh(String refreshToken);
+  Future<void> logOut();
 }
