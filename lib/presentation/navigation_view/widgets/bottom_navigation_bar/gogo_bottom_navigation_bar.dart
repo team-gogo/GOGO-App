59526e44fee1732388b7e9f5ff3f7cecf8f1eeb3
parkyuhyeon <pyh1111802@naver.com>
2025-03-18 13:39:09 +0900

    :recycle: :: GogoBottomNavigationBar 수정

--- lib/presentation/navigation_view/widgets/bottom_navigation_bar/gogo_bottom_navigation_bar.dart
+++ lib/presentation/navigation_view/widgets/bottom_navigation_bar/gogo_bottom_navigation_bar.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:gogo_app/design_system/theme/color.dart';
 import 'package:gogo_app/design_system/theme/icon.dart';
 import 'package:gogo_app/design_system/theme/typography.dart';
-import '../../bloc/gogo_bottom_navigation_bar_bloc.dart';
-import '../../bloc/gogo_bottom_navigation_bar_event.dart';
 
 class GogoBottomNavigationBar extends StatelessWidget {
-  const GogoBottomNavigationBar({super.key});
+  final StatefulNavigationShell navigationShell;
+
+  const GogoBottomNavigationBar({super.key, required this.navigationShell});
 
   @override
   Widget build(BuildContext context) {
@@ -17,57 +18,53 @@ class GogoBottomNavigationBar extends StatelessWidget {
         height: 0,
         color: GogoColors.gray500,
       ),
-      BlocBuilder<GogoBottomNavigationBarBloc, int>(
-        builder: (context, currentIndex) {
-          return BottomNavigationBar(
-            backgroundColor: GogoColors.black,
-            elevation: 0,
-            currentIndex: currentIndex,
-            onTap: (index) {
-              context
-                  .read<GogoBottomNavigationBarBloc>()
-                  .add(GogoBottomNavigationBarEvent.values[index]);
-            },
-            items: [
-              _buildNavItem(
-                icon: GogoIcons.home(
-                  color: currentIndex == 0
-                      ? GogoColors.main600
-                      : GogoColors.gray500,
-                ),
-                label: "홈",
-                isSelected: currentIndex == 0,
-              ),
-              _buildNavItem(
-                icon: GogoIcons.stage(
-                  color: currentIndex == 1
-                      ? GogoColors.main600
-                      : GogoColors.gray500,
-                ),
-                label: "스테이지",
-                isSelected: currentIndex == 1,
-              ),
-              _buildNavItem(
-                icon: GogoIcons.speakerPhone(
-                  color: currentIndex == 2
-                      ? GogoColors.main600
-                      : GogoColors.gray500,
-                ),
-                label: "공지",
-                isSelected: currentIndex == 2,
-              ),
-              _buildNavItem(
-                icon: GogoIcons.person(
-                  color: currentIndex == 3
-                      ? GogoColors.main600
-                      : GogoColors.gray500,
-                ),
-                label: "프로필",
-                isSelected: currentIndex == 3,
-              ),
-            ],
-          );
+      BottomNavigationBar(
+        backgroundColor: GogoColors.black,
+        elevation: 0,
+        currentIndex: navigationShell.currentIndex,
+        onTap: (index) {
+          print("현재 선택된 탭 index: $index");
+
+          navigationShell.goBranch(index);
         },
+        items: [
+          _buildNavItem(
+            icon: GogoIcons.home(
+              color: navigationShell.currentIndex == 0
+                  ? GogoColors.main600
+                  : GogoColors.gray500,
+            ),
+            label: "홈",
+            isSelected: navigationShell.currentIndex == 0,
+          ),
+          _buildNavItem(
+            icon: GogoIcons.stage(
+              color: navigationShell.currentIndex == 1
+                  ? GogoColors.main600
+                  : GogoColors.gray500,
+            ),
+            label: "스테이지",
+            isSelected: navigationShell.currentIndex == 1,
+          ),
+          _buildNavItem(
+            icon: GogoIcons.speakerPhone(
+              color: navigationShell.currentIndex == 2
+                  ? GogoColors.main600
+                  : GogoColors.gray500,
+            ),
+            label: "공지",
+            isSelected: navigationShell.currentIndex == 2,
+          ),
+          _buildNavItem(
+            icon: GogoIcons.person(
+              color: navigationShell.currentIndex == 3
+                  ? GogoColors.main600
+                  : GogoColors.gray500,
+            ),
+            label: "프로필",
+            isSelected: navigationShell.currentIndex == 3,
+          ),
+        ],
       )
     ]);
   }
