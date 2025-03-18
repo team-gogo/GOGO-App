45d2fca3ab855ff44acd41f4bee8824b750f83c5
parkyuhyeon <pyh1111802@naver.com>
2025-03-18 13:43:17 +0900

    :lipstick: :: StageScreen 퍼블리싱 수정

--- lib/presentation/stage/screen/stage_screen.dart
+++ lib/presentation/stage/screen/stage_screen.dart
@@ -16,147 +16,166 @@ class StageScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(vertical: 30),
         child: Column(
-          spacing: 24,
-          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 40,
           children: [
-            Padding(
-              padding: padding,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '참여한 스테이지',
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16,
+              children: [
+                Padding(
+                  padding: padding,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '참여한 스테이지',
+                        style: GogoTypography.body2Extrabold
+                            .copyWith(color: GogoColors.white),
+                      ),
+                      GestureDetector(
+                        child: TagComponent(
+                          color: GogoColors.main400,
+                          padding: const EdgeInsets.all(12.0),
+                          borderRadius: BorderRadius.circular(8),
+                          text: '스테이지 생성',
+                          icon: GogoIcons.plusCircle(
+                            color: GogoColors.main400,
+                            width: 16,
+                            height: 16,
+                          ),
+                          textStyle: GogoTypography.caption2Semibold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SingleChildScrollView(
+                  padding: padding,
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    spacing: 16,
+                    children: [
+                      StageCardComponent(
+                        stageName: '스테이지 이름',
+                        color: GogoColors.gray700,
+                        official: true,
+                        recruiting: true,
+                        manager: true,
+                        broadcast: true,
+                        onTap: () {},
+                        buttonText: '참여하기',
+                        buttonIcon: Container(),
+                      ),
+                      StageCardComponent(
+                        stageName: '스테이지 이름',
+                        color: GogoColors.gray700,
+                        official: true,
+                        recruiting: true,
+                        manager: true,
+                        broadcast: true,
+                        onTap: () {},
+                        buttonText: '참여하기',
+                        buttonIcon: Container(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16,
+              children: [
+                Padding(
+                  padding: padding,
+                  child: Text(
+                    '참여가능한 스테이지',
                     style: GogoTypography.body2Extrabold
                         .copyWith(color: GogoColors.white),
                   ),
-                  GestureDetector(
-                    child: TagComponent(
-                      color: GogoColors.main400,
-                      padding: const EdgeInsets.all(12.0),
-                      borderRadius: BorderRadius.circular(8),
-                      text: '스테이지 생성',
-                      icon: GogoIcons.plusCircle(
-                        color: GogoColors.main400,
-                        width: 16,
-                        height: 16,
+                ),
+                SingleChildScrollView(
+                  padding: padding,
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    spacing: 16,
+                    children: [
+                      StageCardComponent(
+                        stageName: '스테이지 이름',
+                        color: GogoColors.gray700,
+                        official: true,
+                        recruiting: false,
+                        manager: false,
+                        broadcast: true,
+                        onTap: () {},
+                        buttonText: '인증번호로 참여하기',
+                        buttonIcon: GogoIcons.lock(
+                          height: 20.sp,
+                          width: 20.sp,
+                          color: GogoColors.white,
+                        ),
                       ),
-                      textStyle: GogoTypography.caption2Semibold,
-                    ),
-                  )
-                ],
-              ),
+                      StageCardComponent(
+                        stageName: '스테이지 이름',
+                        color: GogoColors.gray700,
+                        official: true,
+                        recruiting: false,
+                        manager: false,
+                        broadcast: true,
+                        onTap: () {},
+                        buttonText: '참여하기',
+                        buttonIcon: Container(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            SingleChildScrollView(
-              padding: padding,
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  StageCardComponent(
-                    stageName: '스테이지 이름',
-                    color: GogoColors.gray700,
-                    official: true,
-                    recruiting: true,
-                    manager: true,
-                    broadcast: true,
-                    onTap: () {},
-                    buttonText: '참여하기',
-                    buttonIcon: Container(),
+            Column(
+              spacing: 16,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: padding,
+                  child: Text(
+                    '모집중인 스테이지',
+                    style: GogoTypography.body2Extrabold
+                        .copyWith(color: GogoColors.white),
                   ),
-                  StageCardComponent(
-                    stageName: '스테이지 이름',
-                    color: GogoColors.gray700,
-                    official: true,
-                    recruiting: true,
-                    manager: true,
-                    broadcast: true,
-                    onTap: () {},
-                    buttonText: '참여하기',
-                    buttonIcon: Container(),
+                ),
+                SingleChildScrollView(
+                  padding: padding,
+                  scrollDirection: Axis.horizontal,
+                  child: Row(                    spacing: 16,
+
+                    children: [
+                      StageCardComponent(
+                        stageName: '스테이지 이름',
+                        color: GogoColors.gray700,
+                        official: true,
+                        recruiting: true,
+                        manager: true,
+                        broadcast: false,
+                        onTap: () {},
+                        buttonText: '참여하기',
+                        buttonIcon: Container(),
+                      ),
+                      StageCardComponent(
+                        stageName: '스테이지 이름',
+                        color: GogoColors.gray700,
+                        official: true,
+                        recruiting: true,
+                        manager: true,
+                        broadcast: true,
+                        onTap: () {},
+                        buttonText: '참여하기',
+                        buttonIcon: Container(),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: padding,
-              child: Text(
-                '참여가능한 스테이지',
-                style: GogoTypography.body2Extrabold
-                    .copyWith(color: GogoColors.white),
-              ),
-            ),
-            SingleChildScrollView(
-              padding: padding,
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  StageCardComponent(
-                    stageName: '스테이지 이름',
-                    color: GogoColors.gray700,
-                    official: true,
-                    recruiting: false,
-                    manager: false,
-                    broadcast: true,
-                    onTap: () {},
-                    buttonText: '인증번호로 참여하기',
-                    buttonIcon: GogoIcons.lock(
-                      height: 20.sp,
-                      width: 20.sp,
-                      color: GogoColors.white,
-                    ),
-                  ),
-                  StageCardComponent(
-                    stageName: '스테이지 이름',
-                    color: GogoColors.gray700,
-                    official: true,
-                    recruiting: false,
-                    manager: false,
-                    broadcast: true,
-                    onTap: () {},
-                    buttonText: '참여하기',
-                    buttonIcon: Container(),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: padding,
-              child: Text(
-                '모집중인 스테이지',
-                style: GogoTypography.body2Extrabold
-                    .copyWith(color: GogoColors.white),
-              ),
-            ),
-            SingleChildScrollView(
-              padding: padding,
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  StageCardComponent(
-                    stageName: '스테이지 이름',
-                    color: GogoColors.gray700,
-                    official: true,
-                    recruiting: true,
-                    manager: true,
-                    broadcast: false,
-                    onTap: () {},
-                    buttonText: '참여하기',
-                    buttonIcon: Container(),
-                  ),
-                  StageCardComponent(
-                    stageName: '스테이지 이름',
-                    color: GogoColors.gray700,
-                    official: true,
-                    recruiting: true,
-                    manager: true,
-                    broadcast: true,
-                    onTap: () {},
-                    buttonText: '참여하기',
-                    buttonIcon: Container(),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
