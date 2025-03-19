a45000001e4da173ed09d3266282350d2a1208b6
youloveli <mymhj122@gmail.com>
2025-03-19 23:51:47 +0900

    :recycle: :: _settingPermission 함수를 main 에서 실행하도록 수정

--- lib/presentation/splash/bloc/splash_bloc.dart
+++ lib/presentation/splash/bloc/splash_bloc.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get_it/get_it.dart';
 import 'package:gogo_app/data/repositories/auth/auth_repository.dart';
 import 'package:gogo_app/presentation/splash/bloc/splash_event.dart';
@@ -11,7 +10,6 @@ class SplashBloc extends Bloc<SplashEvent, SplashState> {
   final AuthRepository _authRepository = GetIt.instance.get<AuthRepository>();
 
   SplashBloc() : super(InitSplashState()) {
-    _settingPermission();
     on<LaunchSplashEvent>((event, emit) async {
       emit(AutoLoginLoading());
       try {
@@ -22,22 +20,5 @@ class SplashBloc extends Bloc<SplashEvent, SplashState> {
         emit(AutoLoginFailed());
       }
     });
-  }
-
-  void _settingPermission() async {
-    ///================================================================= 알림 권한 설정
-    await FirebaseMessaging.instance.requestPermission(
-      alert: true,
-      announcement: true,
-      badge: true,
-      carPlay: true,
-      criticalAlert: false,
-      provisional: false,
-      sound: true,
-    );
-
-    ///================================================================= 포그라운드 알림 처리
-    FirebaseMessaging.onMessage
-        .listen((RemoteMessage message) => print(message.notification?.title));
   }
 }
