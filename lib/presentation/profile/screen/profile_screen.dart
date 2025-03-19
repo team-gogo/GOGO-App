7fbbfe0f0510adb115986e6a46d0e15d49bd06b1
parkyuhyeon <pyh1111802@naver.com>
2025-03-19 20:39:11 +0900

    :truck: :: 변경된 컴포넌트 위치 적용

--- lib/presentation/profile/screen/profile_screen.dart
+++ lib/presentation/profile/screen/profile_screen.dart
@@ -4,7 +4,7 @@ import 'package:gogo_app/design_system/theme/icon.dart';
 import '../../../design_system/component/stage/gogo_stage_card_component.dart';
 import '../../../design_system/theme/color.dart';
 import '../../../design_system/theme/typography.dart';
-import '../widget/component/gogo_profile_card_component.dart';
+import '../widget/gogo_profile_card_component.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -76,7 +76,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
                           broadcast: true,
                           onTap: () {},
                           buttonText: '상세보기',
-                          buttonIcon: GogoIcons.lock(),  color: GogoColors.gray700,
+                          buttonIcon: GogoIcons.lock(),
+                          color: GogoColors.gray700,
                         ),
                       ),
                     )
