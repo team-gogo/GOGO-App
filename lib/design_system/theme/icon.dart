3ee09dff6326c4790cddf52d2ebcf2fe650d002f
parkyuhyeon <pyh1111802@naver.com>
2025-03-20 21:12:59 +0900

    :lipstick: :: Community 아이콘 설정

--- lib/design_system/theme/icon.dart
+++ lib/design_system/theme/icon.dart
@@ -354,6 +354,15 @@ class GogoIcons {
       _buildIcon('dollor_circle.svg',
           color: color, width: width, height: height, onTap: onTap);
 
+  static Widget communit({
+    Color? color,
+    double width = 24,
+    double height = 24,
+    VoidCallback? onTap,
+  }) =>
+      _buildIcon('communit.svg',
+          color: color, width: width, height: height, onTap: onTap);
+
   static Widget coin({
     Color? color,
     double width = 24,
