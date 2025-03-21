20793bbc2fb06065004f153b4e8c02dacca7b372
youloveli <mymhj122@gmail.com>
2025-03-22 08:11:23 +0900

    :fire: :: Sex enum 삭제

--- lib/presentation/sign_up/bloc/sex/sex_state.dart
+++ lib/presentation/sign_up/bloc/sex/sex_state.dart
@@ -1,9 +1,8 @@
-enum Sex { male, female }
-
 abstract class SexState {}
+
 class DisableSexState extends SexState {}
 
-abstract class EnableSexState extends SexState{}
+abstract class EnableSexState extends SexState {}
 
 class EnableMaleSexState extends EnableSexState {}
 
