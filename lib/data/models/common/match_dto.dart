28a99a12f77c3839b64a49479a132652c949d629
parkyuhyeon <pyh1111802@naver.com>
2025-03-25 00:02:53 +0900

    :recycle: :: MatchDto 분리

---
+++ lib/data/models/common/match_dto.dart
@@ -0,0 +1,90 @@
+import 'package:gogo_app/data/models/stage/search_stage/search_betting_stage_response.dart';
+import 'package:json_annotation/json_annotation.dart';
+import '../stage/game_type.dart';
+import '../stage/match_round.dart';
+
+part 'match_dto.g.dart';
+
+@JsonSerializable()
+class MatchDto {
+  final int matchId;
+  final MatchTeam aTeam;
+  final MatchTeam bTeam;
+  final String startDate;
+  final String endDate;
+  final bool isEnd;
+  final MatchRound? round;
+  final GameType category;
+  final String gameName;
+  final System system;
+  final int? turn;
+  final bool isNotice;
+  final Betting betting;
+  final MatchResult? result;
+
+  MatchDto({
+    required this.matchId,
+    required this.aTeam,
+    required this.bTeam,
+    required this.startDate,
+    required this.endDate,
+    required this.isEnd,
+    this.round,
+    required this.category,
+    required this.gameName,
+    required this.system,
+    this.turn,
+    required this.isNotice,
+    required this.betting,
+    this.result,
+  });
+
+  factory MatchDto.fromJson(Map<String, dynamic> json) =>
+      _$MatchDtoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MatchDtoToJson(this);
+}
+
+@JsonSerializable()
+class MatchResult {
+  final int victoryTeamId;
+  final int aTeamScore;
+  final int bTeamScore;
+  final bool? isPredictionSuccess;
+  final int? earnedPoint;
+  final DateTime tempPointExpiredDate;
+
+  MatchResult({
+    required this.victoryTeamId,
+    required this.aTeamScore,
+    required this.bTeamScore,
+    this.isPredictionSuccess,
+    this.earnedPoint,
+    required this.tempPointExpiredDate,
+  });
+
+  factory MatchResult.fromJson(Map<String, dynamic> json) =>
+      _$MatchResultFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MatchResultToJson(this);
+}
+
+@JsonSerializable()
+class MatchTeam {
+  final int teamId;
+  final String teamName;
+  final int bettingPoint;
+  final int? winCount;
+
+  MatchTeam({
+    required this.teamId,
+    required this.teamName,
+    required this.bettingPoint,
+    this.winCount,
+  });
+
+  factory MatchTeam.fromJson(Map<String, dynamic> json) =>
+      _$MatchTeamFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MatchTeamToJson(this);
+}
