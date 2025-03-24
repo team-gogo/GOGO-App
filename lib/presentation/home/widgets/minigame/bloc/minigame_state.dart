4c2dc4c6e51d59173c7f5ce06e49310803c3edb5
parkyuhyeon <pyh1111802@naver.com>
2025-03-24 15:33:30 +0900

    :fire: :: 사용하지않는 MinigameBloc 삭제

--- lib/presentation/home/widgets/minigame/bloc/minigame_state.dart
+++
@@ -1,8 +0,0 @@
-abstract class MiniGameState {}
-
-class MiniGameInitial extends MiniGameState {}
-
-class MiniGameSelected extends MiniGameState {
-  final String? selectedGame;
-  MiniGameSelected(this.selectedGame);
-}
