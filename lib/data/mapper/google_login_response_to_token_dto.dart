e514525c73e13779c32a5c7c41127d02784ce001
parkyuhyeon <pyh1111802@naver.com>
2025-03-25 00:12:05 +0900

    :truck: :: GoogleLoginResponseToTokenDto에 변경된 파일, 클래스 적용

--- lib/data/mapper/google_login_response_to_token_dto.dart
+++ lib/data/mapper/google_login_response_to_token_dto.dart
@@ -1,7 +1,7 @@
 import '../models/auth/google_oauth/token_dto.dart';
-import '../models/auth/sign_in/google_login_response.dart';
+import '../models/auth/sign_in/login_response.dart';
 
-TokenDto toTokenDto(GogoLoginResponse response) {
+TokenDto toTokenDto(LoginResponse response) {
   return TokenDto(
     accessToken: response.accessToken,
     refreshToken: response.refreshToken,
