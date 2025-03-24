74978bb59dbb0805d25458e7a1e2f18e46dca1a5
parkyuhyeon <pyh1111802@naver.com>
2025-03-24 15:33:57 +0900

    :sparkles: :: Router 세팅

--- lib/router.dart
+++ lib/router.dart
@@ -1,10 +1,18 @@
+// router.dart
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:gogo_app/presentation/community/screen/community_main_screen.dart';
+import 'package:gogo_app/presentation/community/screen/community_write_screen.dart';
 import 'package:gogo_app/presentation/home/screen/home_screen.dart';
 import 'package:gogo_app/presentation/logIn/screen/login_screen.dart';
+import 'package:gogo_app/presentation/minigame/screen/coin_toss_screen.dart';
+import 'package:gogo_app/presentation/minigame/screen/minigame_screen.dart';
+import 'package:gogo_app/presentation/minigame/screen/yavarwee_screen.dart';
 import 'package:gogo_app/presentation/navigation_view/widgets/bottom_navigation_bar/gogo_bottom_navigation_bar.dart';
 import 'package:gogo_app/presentation/profile/screen/edit_profile_screen.dart';
 import 'package:gogo_app/presentation/profile/screen/profile_screen.dart';
+import 'package:gogo_app/presentation/ranking/screens/ranking_page.dart';
 import 'package:gogo_app/presentation/sign_up/screen/sign_up_screen.dart';
 import 'package:gogo_app/presentation/splash/screen/splash_screen.dart';
 import 'package:gogo_app/presentation/stage/screen/stage_screen.dart';
@@ -27,6 +35,12 @@ class PageRouter {
   static const String profile = "profile";
   static const String editProfile = "editProfile";
   static const String createStage = "createStage";
+  static const String miniGame = "miniGame";
+  static const String coinToss = "coinToss";
+  static const String yavarwee = "yavarwee";
+  static const String ranking = "ranking";
+  static const String community = "community";
+  static const String communityWrite = "communityWrite";
 
   static GoRoute _customGoRoute({
     required String name,
@@ -34,11 +48,17 @@ class PageRouter {
     List<RouteBase> routes = const [],
   }) {
     return GoRoute(
-        name: name, path: "/$name", routes: routes, builder: (_, __) => screen);
+      name: name,
+      path: "/$name",
+      routes: routes,
+      pageBuilder: (context, state) => CupertinoPage(
+        child: screen,
+      ),
+    );
   }
 
   static final router = GoRouter(
-    initialLocation: "/$home",
+    initialLocation: "/$splash",
     routes: [
       _customGoRoute(name: splash, screen: SplashScreen()),
       GoRoute(
@@ -56,18 +76,29 @@ class PageRouter {
         },
       ),
       _customGoRoute(name: signUp, screen: SignUpScreen()),
-
-      /// ✅ StatefulShellRoute (BottomNavigationBar 포함)
       StatefulShellRoute.indexedStack(
         builder: (_, __, navigationShell) => Scaffold(
           backgroundColor: GogoColors.black,
           body: SafeArea(bottom: false, child: navigationShell),
-          bottomNavigationBar:
-              GogoBottomNavigationBar(navigationShell: navigationShell),
+          bottomNavigationBar: GogoBottomNavigationBar(
+            currentIndex: navigationShell.currentIndex,
+            onTap: (index) {
+              if (index == navigationShell.currentIndex) {
+                if (_.canPop()) {
+                  _.pop();
+                }
+              } else {
+                navigationShell.goBranch(index);
+              }
+            },
+          ),
         ),
         branches: [
-          StatefulShellBranch(
-              routes: [_customGoRoute(name: home, screen: HomeScreen())]),
+          StatefulShellBranch(routes: [
+            _customGoRoute(name: home, screen: HomeScreen(), routes: [
+              _customGoRoute(name: miniGame, screen: MinigameScreen())
+            ])
+          ]),
           StatefulShellBranch(
               routes: [_customGoRoute(name: stage, screen: StageScreen())]),
           StatefulShellBranch(
@@ -76,9 +107,18 @@ class PageRouter {
               routes: [_customGoRoute(name: profile, screen: ProfileScreen())]),
         ],
       ),
-
       _customGoRoute(name: editProfile, screen: EditProfilePage()),
       _customGoRoute(name: createStage, screen: StageCreateScreen()),
+      _customGoRoute(name: coinToss, screen: CoinTossScreen()),
+      _customGoRoute(name: yavarwee, screen: YavarweeScreen()),
+      _customGoRoute(name: ranking, screen: RankingPage()),
+      _customGoRoute(
+        name: community,
+        screen: CommunityMainScreen(),
+        routes: [
+          _customGoRoute(name: communityWrite, screen: CommunityWriteScreen())
+        ],
+      )
     ],
   );
 }
