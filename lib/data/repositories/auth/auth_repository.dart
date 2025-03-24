ee08aa33fcb5fa02d06beb5f00e2acc8994933c6
answad <mymhj122@gmail.com>
2025-03-24 16:04:53 +0900

    :recycle: :: AuthRepository의 googleOAuthLogin 반환값을 TokenDto 에서 Authority으로 변경, toTokenDto mapper 함수를 사용해서 _tokenRepository.saveToken 함수에 tokenDto 전달

--- lib/data/repositories/auth/auth_repository.dart
+++ lib/data/repositories/auth/auth_repository.dart
@@ -1,10 +1,11 @@
 import 'package:gogo_app/data/models/auth/google_oauth/google_oauth_login_request.dart';
 
+import '../../models/auth/additional_sign_in/google_login_response.dart';
 import '../../models/auth/additional_sign_up/additional_sign_up_response.dart';
 import '../../models/auth/google_oauth/token_dto.dart';
 
 abstract class AuthRepository {
-  Future<TokenDto> googleOAuthLogin(
+  Future<Authority> googleOAuthLogin(
       GoogleOAuthLoginRequest body);
   Future<void> additionalSignUp(AdditionalSignUpRequest body);
   Future<TokenDto> tokenRefresh();
--- lib/data/repositories/auth/auth_repository_impl.dart
+++ lib/data/repositories/auth/auth_repository_impl.dart
@@ -4,6 +4,8 @@ import 'package:gogo_app/data/models/auth/additional_sign_up/additional_sign_up_
 import 'package:gogo_app/data/models/auth/google_oauth/google_oauth_login_request.dart';
 
 import '../../data_sources/auth/auth_data_source.dart';
+import '../../mapper/GoogleLoginResponseToTokenDto.dart';
+import '../../models/auth/additional_sign_in/google_login_response.dart';
 import '../../models/auth/google_oauth/token_dto.dart';
 import 'auth_repository.dart';
 
@@ -14,11 +16,11 @@ class AuthRepositoryImpl implements AuthRepository {
   AuthRepositoryImpl(this._authDatasource, this._tokenRepository);
 
   @override
-  Future<TokenDto> googleOAuthLogin(
-      GoogleOAuthLoginRequest body) async {
-    final TokenDto response = await _authDatasource.googleOAuthLogin(body);
-    _tokenRepository.saveToken(response);
-    return response;
+  Future<Authority> googleOAuthLogin(GoogleOAuthLoginRequest body) async {
+    final GoogleLoginResponse response =
+        await _authDatasource.googleOAuthLogin(body);
+    _tokenRepository.saveToken(toTokenDto(response));
+    return response.authority;
   }
 
   @override
