7220be0ceed714cb436cd46defa3009ede829af7
LeeJueon <loolloo080313@gmail.com>
2025-03-25 09:10:59 +0900

    🍱 :: Send 아이콘 추가가

--- lib/design_system/theme/icon.dart
+++ lib/design_system/theme/icon.dart
@@ -363,7 +363,6 @@ class GogoIcons {
       _buildIcon('community.svg',
           color: color, width: width, height: height, onTap: onTap);
 
-
   static Widget coin({
     Color? color,
     double width = 24,
@@ -594,5 +593,14 @@ class GogoIcons {
     VoidCallback? onTap,
   }) =>
       _buildIcon('shop.svg',
+          color: color, width: width, height: height, onTap: onTap);
+
+  static Widget send({
+    Color? color,
+    double width = 24,
+    double height = 24,
+    VoidCallback? onTap,
+  }) =>
+      _buildIcon('send.svg',
           color: color, width: width, height: height, onTap: onTap);
 }
