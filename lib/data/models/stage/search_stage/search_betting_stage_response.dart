4eb579cc22532a253d3128be408c76a069c4959a
parkyuhyeon <pyh1111802@naver.com>
2025-03-25 20:54:37 +0900

    :recycle: :: SearchBettingStageResponseModel 수정

--- lib/data/models/stage/search_stage/search_betting_stage_response.dart
+++ lib/data/models/stage/search_stage/search_betting_stage_response.dart
@@ -1,33 +1,24 @@
-import 'package:gogo_app/data/models/stage/game_type.dart';
+import 'package:gogo_app/data/models/stage/enum_type/game_type.dart';
 import 'package:gogo_app/data/models/common/match_dto.dart';
-import 'package:gogo_app/data/models/stage/match_round.dart';
+import 'package:gogo_app/data/models/stage/enum_type/match_round.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'search_betting_stage_response.g.dart';
 
-enum System {
-  TOURNAMENT,
-  FULL_LEAGUE,
-  SINGLE,
-}
-
 @JsonSerializable()
-class Team {
-  final int teamId;
-  final String teamName;
-  final int bettingPoint;
-  final int winCount;
+class SearchBettingStageResponse {
+  final int count;
+  final List<MatchDto> matches;
 
-  Team({
-    required this.teamId,
-    required this.teamName,
-    required this.bettingPoint,
-    required this.winCount,
+  SearchBettingStageResponse({
+    required this.count,
+    required this.matches,
   });
 
-  factory Team.fromJson(Map<String, dynamic> json) => _$TeamFromJson(json);
+  factory SearchBettingStageResponse.fromJson(Map<String, dynamic> json) =>
+      _$SearchBettingStageResponseFromJson(json);
 
-  Map<String, dynamic> toJson() => _$TeamToJson(this);
+  Map<String, dynamic> toJson() => _$SearchBettingStageResponseToJson(this);
 }
 
 @JsonSerializable()
@@ -46,20 +37,4 @@ class Betting {
       _$BettingFromJson(json);
 
   Map<String, dynamic> toJson() => _$BettingToJson(this);
-}
-
-@JsonSerializable()
-class SearchBettingStageResponse {
-  final int count;
-  final List<MatchDto> matches;
-
-  SearchBettingStageResponse({
-    required this.count,
-    required this.matches,
-  });
-
-  factory SearchBettingStageResponse.fromJson(Map<String, dynamic> json) =>
-      _$SearchBettingStageResponseFromJson(json);
-
-  Map<String, dynamic> toJson() => _$SearchBettingStageResponseToJson(this);
 }
