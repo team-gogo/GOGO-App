3e663be3ffc43ed80278677e3adb849415af2a9a
parkyuhyeon <pyh1111802@naver.com>
2025-03-25 20:46:54 +0900

    :truck: :: GetIt에 변경된 MinigameApi 임포트

--- lib/data/get_it_module/get_it_module.dart
+++ lib/data/get_it_module/get_it_module.dart
@@ -18,12 +18,12 @@ import 'package:gogo_app/data/repositories/mini_game/ws/plinko/plinko_web_socket
 import 'package:gogo_app/data/repositories/mini_game/ws/yavarwee/yavarwee_web_socket_repository.dart';
 import 'package:gogo_app/data/repositories/search_school/search_school_repository.dart';
 import 'package:gogo_app/data/repositories/search_school/search_school_repository_impl.dart';
-import 'package:gogo_app/data/api/mini_game_api.dart';
 import 'package:gogo_app/data/api/stage/stage_api.dart';
 import 'package:gogo_app/data/data_sources/ws/web_socket_data_source.dart';
 import 'package:gogo_app/data/data_sources/ws/web_socket_data_sources_impl.dart';
 import 'package:gogo_app/data/repositories/stage/stage_repository.dart';
 import 'package:gogo_app/data/repositories/stage/stage_repository_impl.dart';
+import '../api/mini_game/mini_game_api.dart';
 import '../data_sources/token_data_source/token_data_source.dart';
 import '../data_sources/token_data_source/token_data_source_impl.dart';
 import '../repositories/mini_game/mini_game_repository.dart';
@@ -47,15 +47,14 @@ void setupDataSourceLocator() {
       () => MiniGameDataSourceImpl(locator<Dio>()));
   locator.registerLazySingleton<SearchSchoolDataSource>(
       () => SearchSchoolDataSourceImpl(locator<Dio>()));
-  locator.registerLazySingleton<TokenDataSource>(
-      () => TokenDataSourceImpl());
+  locator.registerLazySingleton<TokenDataSource>(() => TokenDataSourceImpl());
 
   locator.registerFactory<WebSocketDataSource>(() => WebSocketDataSourceImpl());
 }
 
 void setupRepositoryLocator() {
-  locator.registerLazySingleton<AuthRepository>(
-      () => AuthRepositoryImpl(locator<AuthDatasource>(), locator<TokenDataSource>()));
+  locator.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(
+      locator<AuthDatasource>(), locator<TokenDataSource>()));
   locator.registerLazySingleton<StageRepository>(
       () => StageRepositoryImpl(locator<StageDataSource>()));
   locator.registerLazySingleton<SearchSchoolRepository>(
