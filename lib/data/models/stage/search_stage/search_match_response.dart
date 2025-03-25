654ee2eb1ecb9ef00d6381b6371d6b0df7a9e3c7
parkyuhyeon <pyh1111802@naver.com>
2025-03-25 20:55:44 +0900

    :recycle: :: SearchMatchResponseModel 수정

--- lib/data/models/stage/search_stage/search_match_response.dart
+++ lib/data/models/stage/search_stage/search_match_response.dart
@@ -1,17 +1,17 @@
 import 'package:json_annotation/json_annotation.dart';
 import '../../common/match_dto.dart';
-import '../match_round.dart';
+import '../enum_type/match_round.dart';
 
 part 'search_match_response.g.dart';
 
 @JsonSerializable()
 class SearchMatchResponse {
   final int count;
-  final List<MatchDto> match;
+  final List<MatchDto> matches;
 
   SearchMatchResponse({
     required this.count,
-    required this.match,
+    required this.matches,
   });
 
   factory SearchMatchResponse.fromJson(Map<String, dynamic> json) =>
