506a2fcc67e6b4f0e5c304665e4232a003e4a457
parkyuhyeon <pyh1111802@naver.com>
2025-03-26 13:58:28 +0900

    :recycle: :: Authority 상태에 따라 이동되는 페이지 지정

--- lib/presentation/splash/bloc/splash_bloc.dart
+++ lib/presentation/splash/bloc/splash_bloc.dart
@@ -1,21 +1,29 @@
 import 'dart:developer';
-
 import 'package:bloc/bloc.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:gogo_app/data/models/auth/sign_in/login_response.dart';
 import 'package:gogo_app/data/repositories/auth/auth_repository.dart';
 import 'package:gogo_app/presentation/splash/bloc/splash_event.dart';
 import 'package:gogo_app/presentation/splash/bloc/splash_state.dart';
 
 class SplashBloc extends Bloc<SplashEvent, SplashState> {
   final AuthRepository _authRepository = GetIt.instance.get<AuthRepository>();
+  final FlutterSecureStorage _storage =
+      GetIt.instance.get<FlutterSecureStorage>();
+  static final String _authority = 'authority';
 
   SplashBloc() : super(InitSplashState()) {
     on<LaunchSplashEvent>((event, emit) async {
       emit(AutoLoginLoading());
       try {
         await _authRepository.tokenRefresh();
-        emit(AutoLoginSuccess());
+        final valuePart = await _storage.read(key: _authority);
+        if (valuePart == Authority.USER.toString()) {
+          emit(AutoLoginSuccess());
+        } else {
+          emit(AutoLoginFailed());
+        }
       } catch (e) {
         log("토큰 갱신 실패: $e");
         emit(AutoLoginFailed());
