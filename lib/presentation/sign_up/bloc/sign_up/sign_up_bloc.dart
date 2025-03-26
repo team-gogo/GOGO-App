8fe3304da69e24580fe163ec12d4217d631ba7f4
parkyuhyeon <pyh1111802@naver.com>
2025-03-26 13:56:23 +0900

    :recycle: :: SignUpBloc에 FailsState일때 Exception 핸들링

--- lib/presentation/sign_up/bloc/sign_up/sign_up_bloc.dart
+++ lib/presentation/sign_up/bloc/sign_up/sign_up_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:gogo_app/presentation/sign_up/bloc/sign_up/sign_up_event.dart';
 import 'package:gogo_app/presentation/sign_up/bloc/sign_up/sign_up_state.dart';
@@ -15,10 +16,10 @@ class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
   void _additionalSignUpRequestEventHandler(
       AdditionalSignUpRequestEvent event, Emitter<SignUpState> emit) async {
     try {
-      _authRepository.additionalSignUp(event.additionalSignUpRequest);
+      await _authRepository.additionalSignUp(event.additionalSignUpRequest);
       emit(SignUpSuccess());
     } catch (e) {
-      emit(SignUpFailure());
+      emit(SignUpFailure(e.toString().split('Exception: ')[1]));
     }
   }
 }
