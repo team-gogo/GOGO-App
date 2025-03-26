a6eb079f8f284a416f33ad739f5dac2b0fea576a
parkyuhyeon <pyh1111802@naver.com>
2025-03-26 12:28:29 +0900

    :recycle: :: 학교 필터링 예외처리

--- lib/data/models/search_school/search_school_response.dart
+++ lib/data/models/search_school/search_school_response.dart
@@ -10,7 +10,21 @@ class SearchSchoolRowModel {
   factory SearchSchoolRowModel.fromJson(Map<String, dynamic> json) {
     if (json['schoolInfo'] != null && json['schoolInfo'].length > 1) {
       List rowJson = json['schoolInfo'][1]['row'];
-      List<School> row = rowJson.map((e) => School.fromJson(e)).toList();
+      List<School> row = rowJson
+          .map((e) {
+            try {
+              final school = School.fromJson(e);
+              if (school.type == SchoolType.MIDDLE_SCHOOL ||
+                  school.type == SchoolType.HIGH_SCHOOL) {
+                return school;
+              }
+            } catch (e) {
+              debugPrint('학교 필터링 중 오류 발생: $e');
+            }
+            return null;
+          })
+          .whereType<School>()
+          .toList();
       return SearchSchoolRowModel(row: row);
     } else {
       debugPrintThrottled('값이 없습니다');
