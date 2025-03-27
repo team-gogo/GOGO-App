355afa6ee71ee400229060d2b90ec2932535adb1
LeeJueon <loolloo080313@gmail.com>
2025-03-27 16:05:00 +0900

    💄 :: 버튼 크기 변경경

--- lib/presentation/minigame/widgets/Minigame_description_popup.dart
+++ lib/presentation/minigame/widgets/Minigame_description_popup.dart
@@ -5,10 +5,10 @@ import 'package:gogo_app/design_system/theme/color.dart';
 import 'package:gogo_app/design_system/theme/icon.dart';
 import 'package:gogo_app/design_system/theme/typography.dart';
 
-class MinigameExplanationModal extends StatelessWidget {
+class MinigameDescriptionPopup extends StatelessWidget {
   final String minigameName;
 
-  const MinigameExplanationModal({
+  const MinigameDescriptionPopup({
     required this.minigameName,
     super.key,
   });
@@ -16,9 +16,9 @@ class MinigameExplanationModal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<String> minigameExplanation = [
-      '3개의 컵 중 하나의 컵에 공을 숨기고 섞습니다. 공이 들어가있는 컵을 정확히 찾으면 다음 라운드로 진행합니다! 성공할때마다 X1.1, X1.3, X1.5, X2, X5 배율로 포인트가 지급됩니다.',
+      '3개의 컵 중 하나의 컵에 공을 숨기고 섞습니다.\n공이 들어가있는 컵을 정확히 찾으면 다음 라운드로 진행합니다!\n성공할때마다 X1.1, X1.3, X1.5, X2, X5\n배율로 포인트가 지급됩니다.',
       '앞, 뒷변에 포인트를 배팅하고 예측에 성공하면 포인트를 2배로 얻습니다!',
-      '플레이어가 공을 떨어뜨리면 핀에 부딪히며 랜덤한 경로로 내려갑니다. 최종적으로 도착한 슬롯에 적혀진 만큼의 보상을 얻습니다.'
+      '플레이어가 공을 떨어뜨리면 핀에 부딪히며 랜덤한 경로로 내려갑니다.\n최종적으로 도착한 슬롯에 적혀진 만큼의 보상을 얻습니다.'
     ];
 
     String minigameExplanationSelect() {
@@ -43,8 +43,9 @@ class MinigameExplanationModal extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: 20.r, horizontal: 15.r),
         child: Column(
-          spacing: 12,
+          spacing: 24,
           mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -53,6 +54,7 @@ class MinigameExplanationModal extends StatelessWidget {
                   '게임 설명',
                   style: GogoTypography.body2Semibold.copyWith(
                     color: GogoColors.white,
+                    fontWeight: FontWeight.w800,
                   ),
                 ),
                 GogoIcons.x(
@@ -64,7 +66,8 @@ class MinigameExplanationModal extends StatelessWidget {
               ],
             ),
             Row(
-              spacing: 4,
+              mainAxisSize: MainAxisSize.min,
+              spacing: 12,
               children: [
                 GogoTagComponent(
                   color: GogoColors.main500,
@@ -74,6 +77,11 @@ class MinigameExplanationModal extends StatelessWidget {
                     color: minigameName == '야바위'
                         ? GogoColors.white
                         : GogoColors.main500,
+                    width: 12,
+                    height: 12,
+                  ),
+                  textStyle: GogoTypography.caption1Semibold.copyWith(
+                    fontSize: 11,
                   ),
                 ),
                 GogoTagComponent(
@@ -83,8 +91,13 @@ class MinigameExplanationModal extends StatelessWidget {
                     color: minigameName == '코인토스'
                         ? GogoColors.white
                         : GogoColors.main500,
+                    width: 12,
+                    height: 12,
                   ),
                   tagState: minigameName == '코인토스',
+                  textStyle: GogoTypography.caption1Semibold.copyWith(
+                    fontSize: 11,
+                  ),
                 ),
                 GogoTagComponent(
                   color: GogoColors.main500,
@@ -93,8 +106,17 @@ class MinigameExplanationModal extends StatelessWidget {
                     color: minigameName == '플린코'
                         ? GogoColors.white
                         : GogoColors.main500,
+                    width: 12,
+                    height: 12,
                   ),
                   tagState: minigameName == '플린코',
+                  textStyle: GogoTypography.caption1Semibold.copyWith(
+                    fontSize: 11,
+                  ),
+                ),
+                Flexible(
+                  flex: 3,
+                  child: SizedBox(),
                 ),
               ],
             ),
@@ -102,6 +124,7 @@ class MinigameExplanationModal extends StatelessWidget {
               padding: EdgeInsets.all(8),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8),
+                color: GogoColors.gray600,
               ),
               child: Text(
                 minigameExplanationSelect(),
