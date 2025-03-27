0f98f0d5fc128085b870ca51af847320f1f6c8ea
youloveli <mymhj122@gmail.com>
2025-03-27 19:43:05 +0900

    :recycle: :: detailWidgets 삭제, 파라미터 startDate, gameType, round, system 추가

--- lib/presentation/home/widgets/match_batting_status_dialog.dart
+++ lib/presentation/home/widgets/match_batting_status_dialog.dart
@@ -4,29 +4,40 @@ import 'package:flutter/material.dart';
 import 'package:gogo_app/design_system/component/button/gogo_default_button.dart';
 import 'package:gogo_app/design_system/component/text_field/gogo_text_field.dart';
 
+import '../../../data/models/stage/enum_type/game_type.dart';
+import '../../../data/models/stage/enum_type/match_round.dart';
+import '../../../data/models/stage/enum_type/system_type.dart';
+import '../../../data/util/data_time_formatter.dart';
 import '../../../design_system/component/animattion/animated_int.dart';
+import '../../../design_system/component/tag/gogo_tag_component.dart';
 import '../../../design_system/theme/color.dart';
 import '../../../design_system/theme/icon.dart';
 import '../../../design_system/theme/typography.dart';
 
 class MatchBattingStatusDialog extends StatelessWidget {
-  final List<Widget> detailWidgets;
   final int teamAPoint;
   final int teamBPoint;
   final String teamA;
   final String teamB;
   final bool enableBetting;
+  final DateTime startDate;
+  final GameType gameType;
+  final MatchRound? round;
+  final System system;
   final VoidCallback closeDialog;
   final void Function(String) onBattingClick;
 
   const MatchBattingStatusDialog({
     super.key,
-    required this.detailWidgets,
     required this.teamAPoint,
     required this.teamBPoint,
     required this.teamA,
     required this.teamB,
     required this.enableBetting,
+    required this.startDate,
+    required this.gameType,
+    required this.round,
+    required this.system,
     required this.closeDialog,
     required this.onBattingClick,
   });
@@ -73,7 +84,109 @@ class MatchBattingStatusDialog extends StatelessWidget {
                 ),
                 Row(
                   spacing: 20,
-                  children: [...detailWidgets],
+                  children: [
+                    GogoTagComponent.small(
+                      color: GogoColors.success,
+                      text: formatDateTimeToHourMinute(startDate),
+                      icon: GogoIcons.alarm(
+                        color: GogoColors.success,
+                        width: 12,
+                        height: 12,
+                      ),
+                    ),
+                    switch (system) {
+                      System.TOURNAMENT => GogoTagComponent.small(
+                          color: GogoColors.white,
+                          text: switch (round) {
+                            MatchRound.ROUND_OF_32 => "32",
+                            MatchRound.ROUND_OF_16 => "16",
+                            MatchRound.QUARTER_FINALS => "4",
+                            MatchRound.SEMI_FINALS => "2",
+                            MatchRound.FINALS => "결승전",
+                            null => "",
+                          },
+                          icon: round == MatchRound.FINALS
+                              ? GogoIcons.flame(
+                                  color: GogoColors.white,
+                                  width: 12,
+                                  height: 12,
+                                )
+                              : GogoIcons.trophy(
+                                  color: GogoColors.white,
+                                  width: 12,
+                                  height: 12,
+                                ),
+                        ),
+                      System.FULL_LEAGUE => GogoTagComponent.small(
+                          color: GogoColors.white,
+                          text: "리그전",
+                          icon: GogoIcons.trophy(
+                            color: GogoColors.white,
+                            width: 12,
+                            height: 12,
+                          ),
+                        ),
+                      System.SINGLE => GogoTagComponent.small(
+                          color: GogoColors.white,
+                          text: "단판",
+                          icon: GogoIcons.trophy(
+                            color: GogoColors.white,
+                            width: 12,
+                            height: 12,
+                          ),
+                        ),
+                    },
+                    GogoTagComponent.small(
+                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                      color: GogoColors.main500,
+                      text: switch (gameType) {
+                        GameType.SOCCER => "축구",
+                        GameType.BASKET_BALL => "농구",
+                        GameType.BASE_BALL => "야구",
+                        GameType.VOLLEY_BALL => "배구",
+                        GameType.BADMINTON => "배드민턴",
+                        GameType.LOL => "리그오브레전드",
+                        GameType.ETC => "기타",
+                      },
+                      icon: switch (gameType) {
+                        GameType.SOCCER => GogoIcons.football(
+                            color: GogoColors.main500,
+                            width: 12,
+                            height: 12,
+                          ),
+                        GameType.BASKET_BALL => GogoIcons.basketball(
+                            color: GogoColors.main500,
+                            width: 12,
+                            height: 12,
+                          ),
+                        GameType.BASE_BALL => GogoIcons.baseball(
+                            color: GogoColors.main500,
+                            width: 12,
+                            height: 12,
+                          ),
+                        GameType.VOLLEY_BALL => GogoIcons.volleyball(
+                            color: GogoColors.main500,
+                            width: 12,
+                            height: 12,
+                          ),
+                        GameType.BADMINTON => GogoIcons.badminton(
+                            color: GogoColors.main500,
+                            width: 12,
+                            height: 12,
+                          ),
+                        GameType.LOL => GogoIcons.onlineGame(
+                            color: GogoColors.main500,
+                            width: 12,
+                            height: 12,
+                          ),
+                        GameType.ETC => GogoIcons.volleyball(
+                            color: GogoColors.main500,
+                            width: 12,
+                            height: 12,
+                          ),
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
