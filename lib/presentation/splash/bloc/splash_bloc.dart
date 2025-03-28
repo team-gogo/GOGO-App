b50f9ee996c7ce5cddddf21dbf1ff1ae041c859b
parkyuhyeon <pyh1111802@naver.com>
2025-03-28 12:25:58 +0900

    :recycle: :: Authority의 권한에 따라 화면 이동

--- lib/presentation/splash/bloc/splash_bloc.dart
+++ lib/presentation/splash/bloc/splash_bloc.dart
@@ -9,13 +9,24 @@ import 'package:gogo_app/presentation/splash/bloc/splash_state.dart';
 
 class SplashBloc extends Bloc<SplashEvent, SplashState> {
   final AuthRepository _authRepository = GetIt.instance.get<AuthRepository>();
+  final FlutterSecureStorage _storage =
+      GetIt.instance.get<FlutterSecureStorage>();
+  static final String _authority = 'authority';
 
   SplashBloc() : super(InitSplashState()) {
     on<LaunchSplashEvent>((event, emit) async {
       emit(AutoLoginLoading());
       try {
         await _authRepository.tokenRefresh();
+        final String? authority = await _storage.read(key: _authority);
+        log(authority ?? "authority is null");
+        if (authority != Authority.UNAUTHENTICATED.toString() &&
+            authority != null) {
           emit(AutoLoginSuccess());
+          return;
+        } else {
+          emit(AutoLoginFailed());
+        }
       } catch (e) {
         log("토큰 갱신 실패: $e");
         emit(AutoLoginFailed());
