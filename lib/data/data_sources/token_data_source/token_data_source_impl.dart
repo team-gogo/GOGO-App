ac7a5fb0b46d045eb5126f98ab89323fe915ef35
parkyuhyeon <pyh1111802@naver.com>
2025-03-29 11:32:46 +0900

    :recycle: :: 불필요한 Return 삭제

--- lib/data/data_sources/token_data_source/token_data_source_impl.dart
+++ lib/data/data_sources/token_data_source/token_data_source_impl.dart
@@ -37,9 +37,7 @@ class TokenDataSourceImpl implements TokenDataSource {
     print("🔄 saveToken: ${token.accessToken} ${token.refreshToken}");
     await _storage.write(key: _accessTokenKey, value: token.accessToken);
     await _storage.write(key: _refreshTokenKey, value: token.refreshToken);
-    if (token.authority == null || token.authority == '') {
-      return;
-    } else {
+    if (token.authority != null || token.authority != '') {
       await _storage.write(key: _authority, value: token.authority);
     }
   }
