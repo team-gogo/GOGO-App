fd411870caa3b72a3bac0d7613e17760d51b2c23
parkyuhyeon <pyh1111802@naver.com>
2025-03-30 23:12:33 +0900

    :recycle: :: Authority 조건문 수정

--- lib/data/data_sources/token_data_source/token_data_source_impl.dart
+++ lib/data/data_sources/token_data_source/token_data_source_impl.dart
@@ -37,7 +37,7 @@ class TokenDataSourceImpl implements TokenDataSource {
     print("🔄 saveToken: ${token.accessToken} ${token.refreshToken}");
     await _storage.write(key: _accessTokenKey, value: token.accessToken);
     await _storage.write(key: _refreshTokenKey, value: token.refreshToken);
-    if (token.authority != null && token.authority != '') {
+    if (token.authority?.isNotEmpty == true) {
       await _storage.write(key: _authority, value: token.authority);
     }
   }
