7b2e94b2dd1289eb98930ba881a461143fbb1bc4
LeeJueon <loolloo080313@gmail.com>
2025-03-31 11:25:21 +0900

    ♻️ :: Animation 시간 변경

--- lib/presentation/loadaing_page.dart
+++ lib/presentation/loadaing_page.dart
@@ -21,7 +21,7 @@ class _LoadingPageState extends State<LoadingPage>
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: const Duration(milliseconds: 1500),
+      duration: const Duration(milliseconds: 1250),
       vsync: this,
     )..repeat();
 
