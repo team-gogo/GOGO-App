97fa423e344a4accab80159fea4f03193ddce888
parkyuhyeon <pyh1111802@naver.com>
2025-03-31 20:55:41 +0900

    :fire: :: 사용하지않는 import 삭제

--- lib/presentation/loadaing_page.dart
+++ lib/presentation/loadaing_page.dart
@@ -1,8 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gogo_app/design_system/theme/color.dart';
 
 class LoadingPage extends StatefulWidget {
