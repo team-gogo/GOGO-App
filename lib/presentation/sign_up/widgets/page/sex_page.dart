b24b61a5d216e8e12bf681cfdd12a32d71a05bd5
parkyuhyeon <pyh1111802@naver.com>
2025-03-31 15:40:42 +0900

    :bug: :: Merge과정에서 생긴 충돌 해결

--- lib/presentation/sign_up/widgets/page/sex_page.dart
+++ lib/presentation/sign_up/widgets/page/sex_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:gogo_app/presentation/sign_up/bloc/sex/sex_bloc.dart';
 import '../../../../data/models/auth/additional_sign_up/additional_sign_up_response.dart';
 import '../../../../design_system/component/button/gogo_default_button.dart';
 import '../../../../design_system/theme/color.dart';
 import '../../../../design_system/theme/icon.dart';
 import '../../../../design_system/theme/typography.dart';
+import '../../../../router.dart';
 import '../../bloc/sex/sex_state.dart';
 
 class SexPage extends StatelessWidget {
@@ -37,46 +39,46 @@ class SexPage extends StatelessWidget {
                 curve: Curves.ease,
               ),
             ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Text(
-            '반과 번호를 알려주세요',
-            style: GogoTypography.title4Extrabold
-                .copyWith(color: GogoColors.white),
-          ),
-          SizedBox(
-            height: 36,
-          ),
-          GogoDefaultButton(
-            onTap: () => context.read<SexBloc>().sexController = Sex.MALE,
-            text: "남성",
-            color: state is EnableMaleSexState
-                ? GogoColors.main500
-                : GogoColors.gray400,
-          ),
-          SizedBox(
-            height: 12,
-          ),
-          GogoDefaultButton(
-            onTap: () => context.read<SexBloc>().sexController = Sex.FEMALE,
-            text: "여성",
-            color: state is EnableFemaleSexState
-                ? GogoColors.main500
-                : GogoColors.gray400,
-          ),
-          Spacer(),
-          GogoDefaultButton(
-            onTap: state is EnableSexState
-                ? () => context.goNamed(PageRouter.home)
-                : () {},
-            text: "확인",
-            color: state is EnableSexState
-                ? GogoColors.main600
-                : GogoColors.gray400,
-          ),
-        ],
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              '반과 번호를 알려주세요',
+              style: GogoTypography.title4Extrabold
+                  .copyWith(color: GogoColors.white),
+            ),
+            SizedBox(
+              height: 36,
+            ),
+            GogoDefaultButton(
+              onTap: () => context.read<SexBloc>().sexController = Sex.MALE,
+              text: "남성",
+              color: state is EnableMaleSexState
+                  ? GogoColors.main500
+                  : GogoColors.gray400,
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            GogoDefaultButton(
+              onTap: () => context.read<SexBloc>().sexController = Sex.FEMALE,
+              text: "여성",
+              color: state is EnableFemaleSexState
+                  ? GogoColors.main500
+                  : GogoColors.gray400,
+            ),
+            Spacer(),
+            GogoDefaultButton(
+              onTap: state is EnableSexState
+                  ? () => context.goNamed(PageRouter.home)
+                  : () {},
+              text: "확인",
+              color: state is EnableSexState
+                  ? GogoColors.main600
+                  : GogoColors.gray400,
+            ),
+          ],
+        ),
       );
     });
   }
