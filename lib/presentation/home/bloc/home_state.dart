7991ab4710fca902fa1d385e0feee0440eeab291
answad <mymhj122@gmail.com>
2025-04-02 20:46:21 +0900

    :sparkles: :: HomeState 추가

---
+++ lib/presentation/home/bloc/home_state.dart
@@ -0,0 +1,9 @@
+abstract class HomeState {}
+
+class InitialHomeState extends HomeState {}
+
+class LoadingMatchHomeState extends HomeState {}
+
+class LoadingHomeState extends HomeState {}
+
+class ErrorHomeState extends HomeState {}
