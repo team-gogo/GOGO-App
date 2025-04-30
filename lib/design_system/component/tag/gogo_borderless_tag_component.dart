fd2d23fdb6098343e513cc20cec564836b6b6439
USer <user@user.com>
2025-04-30 19:56:58 +0900

    💄:: spacing 오류 수정

--- lib/design_system/component/tag/gogo_borderless_tag_component.dart
+++ lib/design_system/component/tag/gogo_borderless_tag_component.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:gogo_app/design_system/theme/icon.dart';
-
-import '../../theme/color.dart';
 import '../../theme/typography.dart';
 
 class GogoBorderlessTagComponent extends StatelessWidget {
   final Color color;
 
-  final EdgeInsetsGeometry padding;
   final double spacing;
   final Widget icon;
   final String text;
@@ -18,22 +14,18 @@ class GogoBorderlessTagComponent extends StatelessWidget {
     required this.color,
     required this.text,
     this.icon = const SizedBox(),
-    this.padding = const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
     this.spacing = 4,
     this.textStyle = GogoTypography.caption1Semibold,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: padding,
-      child:ShaderMask(
+    return ShaderMask(
         shaderCallback: (bounds) => LinearGradient(
           colors: [color,color],
         ).createShader(bounds),
         blendMode: BlendMode.srcIn,
         child: Row(
-          spacing: spacing,
           children: [
             icon,
             SizedBox(width: spacing),
@@ -46,7 +38,6 @@ class GogoBorderlessTagComponent extends StatelessWidget {
             ),
           ],
         ),
-      )
     );
   }
 }
