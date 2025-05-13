aa078f89d8a4cec35fcbbb7761b94cf83e18eaa8
aiden30015 <loolloo080313@gmail.com>
2025-05-13 17:08:54 +0900

    ✨ :: IsLiked가 null일시 false로 기본값 설정

--- lib/data/models/stage/community/community_like_response.dart
+++ lib/data/models/stage/community/community_like_response.dart
@@ -4,10 +4,13 @@ part 'community_like_response.g.dart';
 
 @JsonSerializable()
 class CommunityLikeResponse {
+  @JsonKey(defaultValue: false) 
   final bool isLiked;
 
   CommunityLikeResponse({required this.isLiked});
 
-  factory CommunityLikeResponse.fromJson(Map<String, dynamic> json) => _$CommunityLikeResponseFromJson(json);
+  factory CommunityLikeResponse.fromJson(Map<String, dynamic> json) =>
+      _$CommunityLikeResponseFromJson(json);
+
   Map<String, dynamic> toJson() => _$CommunityLikeResponseToJson(this);
 }
