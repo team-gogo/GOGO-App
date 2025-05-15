5efd2d706423b540fabdd2832bb43a81c8a90bb8
parkyuhyeon <pyh1111802@naver.com>
2025-05-15 11:12:36 +0900

    :recycle: :: Ranking Bloc 로직 변경

--- lib/presentation/ranking/bloc/ranking_event.dart
+++ lib/presentation/ranking/bloc/ranking_event.dart
@@ -1,7 +1,7 @@
 abstract class RankingEvent {}
-
 class GetRanking extends RankingEvent {
   final int stageId;
+  final bool isRefresh;
 
-  GetRanking({required this.stageId});
+  GetRanking({required this.stageId, this.isRefresh = false});
 }
