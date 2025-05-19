e0236630682d1af5b97cd8add0201ea6dc2667bb
aiden30015 <loolloo080313@gmail.com>
2025-05-20 08:43:24 +0900

    ✨ :: api 화면 연결

--- lib/presentation/profile/screen/profile_screen.dart
+++ lib/presentation/profile/screen/profile_screen.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gogo_app/data/models/auth/additional_sign_up/additional_sign_up_response.dart';
+import 'package:gogo_app/data/models/stage/search_stage/search_stage_response.dart';
+import 'package:gogo_app/design_system/component/stage/gogo_stage_card_component.dart';
 import 'package:gogo_app/design_system/theme/icon.dart';
+import 'package:gogo_app/presentation/loading/loadaing_page.dart';
 import 'package:gogo_app/presentation/profile/bloc/profile_bloc.dart';
 import 'package:gogo_app/presentation/profile/bloc/profile_event.dart';
 import 'package:gogo_app/presentation/profile/bloc/profile_state.dart';
@@ -19,7 +22,7 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider<ProfileBloc>(
-      create: (BuildContext context) => ProfileBloc(),
+      create: (BuildContext context) => ProfileBloc()..add(FetchMyProfile()),
       child: ProfileContentScreen(),
       );
   }
@@ -44,182 +47,191 @@ class _ProfileContentState extends State<ProfileContentScreen> {
   );
 
   @override
-  void initState() {
-    super.initState();
-    context.read<ProfileBloc>().add(FetchUserInfo());
-  }
-
-  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => setState(() {
         selected = false;
       }),
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-          child: SingleChildScrollView(
-            child: Stack(
-              alignment: Alignment.topRight,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      child: BlocBuilder<ProfileBloc,ProfileState>(
+        buildWhen: (previous, current) {
+          return previous != current;
+        },
+        builder: (context,state) {
+          if(state is ProfileLoadingState) {
+            return LoadingPage();
+          }
+
+          else if(state is ProfileLoadedState) {
+                List<Stage> participatedStages = [];
+                List<Stage> recruitingStage = [];
+                List<Stage> confirmedStages = [];
+                for (int i = 0; i < state.searchStageResponse.count; i++) {
+                  if (state.searchStageResponse.stages[i].isParticipating) {
+                    participatedStages.add(state.searchStageResponse.stages[i]);
+                  }
+                  if (state.searchStageResponse.stages[i].status == StageStatus.CONFIRMED) {
+                    confirmedStages.add(state.searchStageResponse.stages[i]);
+                  }
+                  if (state.searchStageResponse.stages[i].status == StageStatus.RECRUITING) {
+                    recruitingStage.add(state.searchStageResponse.stages[i]);
+                  }
+                }
+                
+          return Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+              child: SingleChildScrollView(
+                child: Stack(
+                  alignment: Alignment.topRight,
                   children: [
-                    Text(
-                      '내 정보',
-                      style: GogoTypography.body2Extrabold
-                          .copyWith(color: GogoColors.white),
-                    ),
-                    SizedBox(
-                      height: 22,
-                    ),
-                    BlocBuilder<ProfileBloc,ProfileState>(
-                      builder: (context, state) {
-                        if(state is UserInfoLoadingState) {
-                          return Center(
-                            child: CircularProgressIndicator(color: GogoColors.main500,),
-                          );
-                        }
-    
-                        else if(state is UserInfoLoadedState) {
-                          return ProfileCardComponent(
-                      name: state.response.name,
-                      school: state.response.schoolName,
-                      male: state.response.sex == Sex.MALE ? "남자" : "여자",
-                    );
-                        }
-    
-                        else if(state is UserInfoErrorState) {
-                          return Center(
-                            child: Text(state.message,style: TextStyle(color: Colors.white),),
-                          );
-                        }
-    
-                        else {
-                          return Text("데이터 없음",style: TextStyle(color: Colors.white),);
-                        }
-                      }
-                      ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Text(
-                      '내가 참여한 스테이지',
-                      style: GogoTypography.body2Extrabold
-                          .copyWith(color: GogoColors.white),
-                    ),
-                    SizedBox(
-                      height: 22,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '내 정보',
+                          style: GogoTypography.body2Extrabold
+                              .copyWith(color: GogoColors.white),
+                        ),
+                        SizedBox(
+                          height: 22,
+                        ),
+                        ProfileCardComponent(
+                          name: state.userInfoResponse.name,
+                          school: state.userInfoResponse.schoolName,
+                          male: state.userInfoResponse.sex == Sex.MALE ? "남자" : "여자",
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Text(
+                          '내가 참여한 스테이지',
+                          style: GogoTypography.body2Extrabold
+                              .copyWith(color: GogoColors.white),
+                        ),
+                        SizedBox(
+                          height: 22,
+                        ),
+                        Column(
+                                spacing: 22,
+                                children: List.generate(
+                                  participatedStages.length,
+                                  (index) => GogoStageCardComponent(
+                                      stage: participatedStages[index],
+                                      color: GogoColors.gray700,
+                                      broadcast: false,
+                                    ),
+                                ),
+                              ),
+                      ],
                     ),
                     Column(
-                      spacing: 22,
-                      // children: List.generate(
-                      //   10,
-                      //   (index) => GogoStageCardComponent(
-                      //
-                      //     broadcast: true,
-                      //     onTap: () {},
-                      //     color: GogoColors.gray700,
-                      //   ),
-                      // ),
-                    )
-                  ],
-                ),
-                Column(
-                  spacing: 8,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    GestureDetector(
-                      onTap: () => setState(() {
-                        selected = !selected;
-                      }),
-                      child: Container(
-                        decoration: BoxDecoration(),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          spacing: 8,
-                          children: [
-                            GogoIcons.gearWheel(
-                              width: 24,
-                              height: 24,
-                              color: selected
-                                  ? GogoColors.white
-                                  : GogoColors.gray500,
+                      spacing: 8,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        GestureDetector(
+                          onTap: () => setState(() {
+                            selected = !selected;
+                          }),
+                          child: Container(
+                            decoration: BoxDecoration(),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              spacing: 8,
+                              children: [
+                                GogoIcons.gearWheel(
+                                  width: 24,
+                                  height: 24,
+                                  color: selected
+                                      ? GogoColors.white
+                                      : GogoColors.gray500,
+                                ),
+                                Text(
+                                  '설정',
+                                  style: selected
+                                      ? subjectStyle.copyWith(
+                                      color: GogoColors.white)
+                                      : subjectStyle,
+                                ),
+                              ],
                             ),
-                            Text(
-                              '설정',
-                              style: selected
-                                  ? subjectStyle.copyWith(
-                                  color: GogoColors.white)
-                                  : subjectStyle,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    if (selected)
-                      Container(
-                        padding: EdgeInsets.all(24),
-                        width: 108,
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Color(0xff000000),
-                              blurRadius: 9,
-                            ),
-                          ],
-                          color: GogoColors.gray700,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(12),
                           ),
                         ),
-                        child: Column(
-                          spacing: 30,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            GestureDetector(
-                              onTap: () =>
-                                  context.pushNamed(PageRouter.editProfile),
-                              child: Container(
-                                width: double.infinity,
-                                decoration: BoxDecoration(),
-                                alignment: Alignment.center,
-                                child: Text(
-                                  '정보수정',
-                                  style: GogoTypography.body3Semibold.copyWith(
-                                    color: GogoColors.white,
-                                  ),
+                        if (selected)
+                          Container(
+                            padding: EdgeInsets.all(24),
+                            width: 108,
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color(0xff000000),
+                                  blurRadius: 9,
                                 ),
+                              ],
+                              color: GogoColors.gray700,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(12),
                               ),
                             ),
-                            Divider(),
-                            GestureDetector(
-                              child: Container(
-                                width: double.infinity,
-                                decoration: BoxDecoration(),
-                                alignment: Alignment.center,
-                                child: Text(
-                                  '회원 탈퇴',
-                                  style: GogoTypography.body3Semibold.copyWith(
-                                    color: GogoColors.error,
+                            child: Column(
+                              spacing: 30,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                GestureDetector(
+                                  onTap: () =>
+                                      context.pushNamed(PageRouter.editProfile),
+                                  child: Container(
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(),
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      '정보수정',
+                                      style: GogoTypography.body3Semibold.copyWith(
+                                        color: GogoColors.white,
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
+                                Divider(),
+                                GestureDetector(
+                                  child: Container(
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(),
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      '회원 탈퇴',
+                                      style: GogoTypography.body3Semibold.copyWith(
+                                        color: GogoColors.error,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                      )
-                    else
-                      SizedBox.shrink()
+                          )
+                        else
+                          SizedBox.shrink()
+                      ],
+                    ),
                   ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+          }
+
+          else if(state is ProfileErrorState) {
+            return Center(
+              child: Text(state.message,style: TextStyle(color: Colors.white),),
+            );
+          }
+          
+          else {
+            return Container();
+          }
+        }
       ),
     );
   }
