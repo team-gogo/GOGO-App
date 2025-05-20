2a6c0d125070a862a209d3039fa1fe26258eafdf
parkyuhyeon <pyh1111802@naver.com>
2025-05-20 20:00:02 +0900

    :recycle: :: Match Detail Screen 스페이스 주석 처리

--- lib/presentation/match_detail/screen/match_detail_screen.dart
+++ lib/presentation/match_detail/screen/match_detail_screen.dart
@@ -248,10 +248,13 @@ class MatchDetailScreen extends StatelessWidget {
       GameType.SOCCER => GogoIcons.footballFullMap(),
       GameType.BASKET_BALL => GogoIcons.basketballFullMap(),
       GameType.BASE_BALL => GogoIcons.baseballFullMap(),
-      GameType.VOLLEY_BALL => GogoIcons.volleyBallFullMap(),
+      GameType.VOLLEY_BALL =>
+        Container(color: GogoColors.gray600), // TODO: 이미지 삽입 해야합니다
       GameType.BADMINTON => GogoIcons.badmintonFullMap(),
-      GameType.LOL => Container(color: GogoColors.gray600),
-      GameType.ETC => Container(color: GogoColors.gray600),
+      GameType.LOL =>
+        Container(color: GogoColors.gray600), // TODO: 이미지 삽입 해야합니다
+      GameType.ETC =>
+        Container(color: GogoColors.gray600), // TODO: 이미지 삽입 해야합니다
     };
     final fullWidth = MediaQuery.of(context).size.width * 2;
     final fullHeight = fullWidth;
