a42b80a4ea66b032509c4a51b5909f3d76e6115a
aiden30015 <loolloo080313@gmail.com>
2025-05-27 20:10:29 +0900

    ✨ :: 포인트 및 티켓 수 값 받도록 추가

--- lib/presentation/minigame/screen/yavarwee_screen.dart
+++ lib/presentation/minigame/screen/yavarwee_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:gogo_app/presentation/minigame/widgets/minigame_component.dart';
 
 class YavarweeScreen extends StatelessWidget {
-  const YavarweeScreen({super.key});
+
+  final int point;
+  final int ticketsCount;
+
+  const YavarweeScreen({super.key, required this.point, required this.ticketsCount});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            MinigameComponent(
+              point: point, 
+              ticketsCount: ticketsCount, 
+              action: '섞기',
+              controller: TextEditingController(),
+              )
+          ],
+        ),
+      ),
     );
   }
 }
