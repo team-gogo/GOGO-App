c68d1dae7196147b02a011af3c3fb38e44fbc278
youloveli <mymhj122@gmail.com>
2025-05-29 08:36:01 +0900

    :sparkles: :: item  Loading 기능이 있는 MatchListBloc 생성

---
+++ lib/presentation/match_list/bloc/match_list_bloc.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:gogo_app/data/models/stage/enum_type/game_type.dart';
+import '../../../data/repositories/stage/stage_repository.dart';
+import 'match_list_event.dart';
+import 'match_list_state.dart';
+import '../../../data/models/common/match_dto.dart';
+
+class MatchListBloc extends Bloc<MatchListEvent, MatchListState> {
+  final StageRepository _stageRepository = GetIt.instance<StageRepository>();
+
+  final int stageId;
+  final int year;
+  final int month;
+  final int day;
+  GameType? gameType;
+
+  MatchListBloc({
+    required this.stageId,
+    required this.year,
+    required this.month,
+    required this.day,
+  }) : super(InitMatchList()) {
+    on<LoadItems>(_onLoadItems);
+  }
+
+  Future<void> _onLoadItems(
+      LoadItems event, Emitter<MatchListState> emit) async {
+    emit(LoadingMatchList());
+
+    try {
+      final matchesResponse = await _stageRepository.searchMatch(
+        stageId,
+        year,
+        month,
+        day,
+      );
+      List<MatchDto> filtered = matchesResponse.matches;
+
+      if (event.gameType != null) {
+        filtered =
+            filtered.where((e) => event.gameType == event.gameType).toList();
+      }
+      if (event.sortOrder != null) {
+        filtered.sort((a, b) {
+          return event.sortOrder == SortOrder.ascending
+              ? a.startDate.compareTo(b.startDate)
+              : b.startDate.compareTo(a.startDate);
+        });
+      }
+
+      emit(LoadedMatchList(matchList: filtered, hasReachedMax: true));
+    } catch (e) {
+      emit(InitMatchList());
+    }
+  }
+}
