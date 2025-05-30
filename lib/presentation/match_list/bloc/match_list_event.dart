0820ec940aaef353a5caa2be962dd7dd47bd3740
youloveli <mymhj122@gmail.com>
2025-05-30 14:31:07 +0900

    :sparkles: :: batting event 추가

--- lib/presentation/match_list/bloc/match_list_event.dart
+++ lib/presentation/match_list/bloc/match_list_event.dart
@@ -1,5 +1,7 @@
 import 'package:gogo_app/data/models/stage/enum_type/game_type.dart';
 
+import '../../../data/models/betting/request/betting_match_request.dart';
+
 abstract class MatchListEvent {}
 
 class LoadItems extends MatchListEvent {
@@ -10,3 +12,13 @@ class LoadItems extends MatchListEvent {
 }
 
 enum SortOrder { ascending, descending }
+
+class BettingMatch extends MatchListEvent {
+  final int matchId;
+  final BettingMatchRequest request;
+
+  BettingMatch({
+    required this.matchId,
+    required this.request,
+  });
+}
